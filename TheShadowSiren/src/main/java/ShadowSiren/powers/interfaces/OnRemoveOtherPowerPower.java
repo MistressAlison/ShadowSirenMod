edacb9e526acc3db055ac39df08a2f7c15bcc629
MistressAlison <alisonmoons@gmail.com>
2021-05-02 21:21:24 -0300

    Added new interface and accompanying patch for powers that do something when a different power is removed from their owner

---
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/interfaces/OnRemoveOtherPowerPower.java
@@ -0,0 +1,7 @@
+package ShadowSiren.powers.interfaces;
+
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public interface OnRemoveOtherPowerPower {
+    void onRemoveOtherPower(AbstractPower powerRemoved);
+}

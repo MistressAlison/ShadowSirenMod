ec2a278df01a1793dcc79ce9df7f04ff9243fccd
MistressAlison <alisonmoons@gmail.com>
2021-05-02 21:20:54 -0300

    Burn OnLostHPLast patch removed

--- TheShadowSiren/src/main/java/ShadowSiren/patches/OnLoseHPLastPatches.java
+++ TheShadowSiren/src/main/java/ShadowSiren/patches/OnLoseHPLastPatches.java
@@ -39,7 +39,7 @@ public class OnLoseHPLastPatches {
     public static class MonsterDamageTakenListener {
         @SpireInsertPatch(locator = Locator.class, localvars = {"damageAmount"})
         public static SpireReturn<?> damageTakenListener(AbstractMonster __instance, DamageInfo info, @ByRef int[] damageAmount) {
-            if (__instance.hasPower(BurnPower.POWER_ID)) {
+            /*if (__instance.hasPower(BurnPower.POWER_ID)) {
                 //If it has already activated the suicide sequence, we ignore all future damage actions at this point
                 if (((BurnPower)__instance.getPower(BurnPower.POWER_ID)).activated) {
                     return SpireReturn.Return(null);
@@ -47,7 +47,7 @@ public class OnLoseHPLastPatches {
                     //Else check to see if the suicide action should trigger
                     ((BurnPower)__instance.getPower(BurnPower.POWER_ID)).deathCheck(damageAmount[0]);
                 }
-            }
+            }*/
             return SpireReturn.Continue();
         }
         private static class Locator extends SpireInsertLocator {

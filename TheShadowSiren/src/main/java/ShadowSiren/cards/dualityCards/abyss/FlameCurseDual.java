642e271a4a92fe204ad5a5e7f71758726edf9cec
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:45:24 -0300

    Added Abyssal Duality to FlameCurse

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/abyss/FlameCurseDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/abyss/FlameCurseDual.java
@@ -1,11 +1,10 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.abyss;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractAbyssCard;
-import ShadowSiren.cards.dualityCards.abyss.FlameCurseDual;
-import ShadowSiren.cards.interfaces.VigorMagicBuff;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.BurnPower;
+import ShadowSiren.powers.ChillPower;
 import ShadowSiren.powers.HexingPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
@@ -15,11 +14,11 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class FlameCurse extends AbstractAbyssCard implements VigorMagicBuff {
+public class FlameCurseDual extends AbstractAbyssCard implements UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(FlameCurse.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(FlameCurseDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderSkill.png");
 
     // /TEXT DECLARATION/
@@ -32,15 +31,14 @@ public class FlameCurse extends AbstractAbyssCard implements VigorMagicBuff {
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int BURN = 3;
+    private static final int CHILL = 3;
     private static final int HEX = 3;
 
     // /STAT DECLARATION/
 
-    //TODO buff
-    public FlameCurse() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new FlameCurseDual());
-        magicNumber = baseMagicNumber = BURN;
+    public FlameCurseDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = CHILL;
         secondMagicNumber = baseSecondMagicNumber = HEX;
         exhaust = true;
     }
@@ -51,12 +49,12 @@ public class FlameCurse extends AbstractAbyssCard implements VigorMagicBuff {
         if (upgraded) {
             for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
                 if (!aM.isDeadOrEscaped()) {
-                    this.addToBot(new ApplyPowerAction(aM, p, new BurnPower(aM, p, magicNumber)));
+                    this.addToBot(new ApplyPowerAction(aM, p, new ChillPower(aM, magicNumber)));
                     this.addToBot(new ApplyPowerAction(aM, p, new HexingPower(aM, p, secondMagicNumber)));
                 }
             }
         } else {
-            this.addToBot(new ApplyPowerAction(m, p, new BurnPower(m, p, magicNumber)));
+            this.addToBot(new ApplyPowerAction(m, p, new ChillPower(m, magicNumber)));
             this.addToBot(new ApplyPowerAction(m, p, new HexingPower(m, p, secondMagicNumber)));
         }
     }

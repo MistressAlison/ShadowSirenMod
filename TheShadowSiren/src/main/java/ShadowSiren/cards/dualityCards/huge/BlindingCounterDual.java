2e1410de7d3e2aa5aa822ae98acc6c22adb80c21
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:42:34 -0300

    Added Huge Duality to Blinding Counter

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/BlindingCounterDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/BlindingCounterDual.java
@@ -1,21 +1,21 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.huge;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractHugeCard;
-import ShadowSiren.cards.dualityCards.huge.BlindingCounterDual;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.BlindingCounterPower;
+import ShadowSiren.powers.TrippingCounterPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class BlindingCounter extends AbstractHugeCard {
+public class BlindingCounterDual extends AbstractHugeCard implements UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(BlindingCounter.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(BlindingCounterDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderPower.png");
 
     // /TEXT DECLARATION/
@@ -33,14 +33,14 @@ public class BlindingCounter extends AbstractHugeCard {
     // /STAT DECLARATION/
 
 
-    public BlindingCounter() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new BlindingCounterDual());
+    public BlindingCounterDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new BlindingCounterPower(p, 1)));
+        this.addToBot(new ApplyPowerAction(p, p, new TrippingCounterPower(p, 1)));
     }
 
     //Upgraded stats.

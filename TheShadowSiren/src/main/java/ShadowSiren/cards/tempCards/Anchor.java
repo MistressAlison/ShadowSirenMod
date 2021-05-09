35985dda2352b2e9a6c7f169fd1f6c469cd82340
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:39:05 -0300

    Anchor and Harbor reworked

--- TheShadowSiren/src/main/java/ShadowSiren/cards/tempCards/Anchor.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/tempCards/Anchor.java
@@ -5,7 +5,11 @@ import ShadowSiren.actions.ImmediateExhaustCardAction;
 import ShadowSiren.cards.abstractCards.AbstractAbyssCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.stances.AbyssStance;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -24,36 +28,38 @@ public class Anchor extends AbstractAbyssCard implements TempCard {
     // STAT DECLARATION
 
     private static final AbstractCard.CardRarity RARITY = CardRarity.UNCOMMON;
-    private static final AbstractCard.CardTarget TARGET = CardTarget.NONE;
-    private static final AbstractCard.CardType TYPE = CardType.STATUS;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
+    private static final AbstractCard.CardType TYPE = CardType.ATTACK;
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
+    private static final int DAMAGE = 2;
+    private static final int BLOCK = 2;
+    private static final int RETAIN_BOOST = 1;
+    private static final int UPGRADE_PLUS_RETAIN_BOOST = 1;
 
     // /STAT DECLARATION/
 
-
     public Anchor() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        damage = baseDamage = DAMAGE;
+        block = baseBlock = BLOCK;
+        magicNumber = baseMagicNumber = RETAIN_BOOST;
         exhaust = true;
         selfRetain = true;
     }
 
     // Actions the card should do.
     @Override
-    public void use(AbstractPlayer p, AbstractMonster m) {}
-
-    @Override
-    public void triggerOnGlowCheck() {
-        super.triggerOnGlowCheck();
-        if (!AbstractDungeon.player.stance.ID.equals(AbyssStance.STANCE_ID)) {
-            this.addToTop(new ImmediateExhaustCardAction(this));
-        }
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        this.addToBot(new GainBlockAction(p, p, block));
     }
 
     @Override
     public void onRetained() {
-        super.onRetained();
+        this.upgradeDamage(this.magicNumber);
+        this.upgradeBlock(this.magicNumber);
     }
 
     //Upgraded stats.
@@ -61,7 +67,7 @@ public class Anchor extends AbstractAbyssCard implements TempCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            //rawDescription = UPGRADE_DESCRIPTION;
+            upgradeMagicNumber(UPGRADE_PLUS_RETAIN_BOOST);
             initializeDescription();
             super.upgrade();
         }

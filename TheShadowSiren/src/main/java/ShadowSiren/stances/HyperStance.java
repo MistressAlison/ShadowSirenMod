fa1dc0d92fa44fd0a04725b85b58545139d6442a
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:57:25 -0300

    Hyper completely reworked. Now also an OnLoseHPStance that lowers damage taken. If you successfully take no damage in a round, you gain 1 Charge.

--- TheShadowSiren/src/main/java/ShadowSiren/stances/HyperStance.java
+++ TheShadowSiren/src/main/java/ShadowSiren/stances/HyperStance.java
@@ -5,7 +5,6 @@ import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.ChargePower;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -13,12 +12,15 @@ import com.megacrit.cardcrawl.localization.StanceStrings;
 import com.megacrit.cardcrawl.stances.AbstractStance;
 import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
 
-public class HyperStance extends AbstractStance {
+public class HyperStance extends AbstractStance implements OnLoseHPStance {
     public static final String STANCE_ID = ShadowSirenMod.makeID("HyperStance");
     private static final StanceStrings stanceString = CardCrawlGame.languagePack.getStanceString(STANCE_ID);
     private static long sfxId = -1L;
-    private static final int CHARGE_AMOUNT = 2;
+    private static final int CHARGE_AMOUNT = 1;
+    private static final int DAMAGE_REDUCTION = 2;
+    private boolean tookDamage;
 
+    //TODO Steadfast? Zealous?
     public HyperStance() {
         this.ID = STANCE_ID;
         this.name = stanceString.NAME;
@@ -45,12 +47,12 @@ public class HyperStance extends AbstractStance {
     public void onExitStance() {
         this.stopIdleSfx();
         //((Vivian) AbstractDungeon.player).getAnimation().myPlayer.setScale(((Vivian) AbstractDungeon.player).getAnimation().myPlayer.getScale()/FACTOR);
-        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ChargePower(AbstractDungeon.player, CHARGE_AMOUNT)));
+        /*AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ChargePower(AbstractDungeon.player, CHARGE_AMOUNT)));
         AbstractDungeon.player.tint.changeColor(Color.WHITE.cpy());
         AbstractDungeon.actionManager.addToTop(new SFXAction("ORB_LIGHTNING_CHANNEL", 0.1F));
         if (AbstractDungeon.player instanceof Vivian) {
             ((Vivian) AbstractDungeon.player).playAnimation("idle");
-        }
+        }*/
     }
 
     public void stopIdleSfx() {
@@ -60,16 +62,33 @@ public class HyperStance extends AbstractStance {
         }
     }
 
-    public float atDamageGive(float damage, DamageInfo.DamageType type) {
+    /*public float atDamageGive(float damage, DamageInfo.DamageType type) {
         return type == DamageInfo.DamageType.NORMAL ? damage * 1.2F : damage;
     }
 
     public float atDamageReceive(float damage, DamageInfo.DamageType type) {
         return type == DamageInfo.DamageType.NORMAL ? damage * 0.8F : damage;
+    }*/
+
+    @Override
+    public void atStartOfTurn() {
+        if(!tookDamage) {
+            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ChargePower(AbstractDungeon.player, CHARGE_AMOUNT)));
+        }
+        tookDamage = false;
     }
 
     @Override
     public void updateDescription() {
         this.description = stanceString.DESCRIPTION[0];
+    }
+
+    @Override
+    public int onLoseHP(DamageInfo info, int damageAmount) {
+        damageAmount = Math.max(0, damageAmount - DAMAGE_REDUCTION);
+        if (damageAmount > 0) {
+            tookDamage = true;
+        }
+        return damageAmount;
     }
 }

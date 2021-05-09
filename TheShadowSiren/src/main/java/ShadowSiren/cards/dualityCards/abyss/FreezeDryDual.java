e5d1baef99e418bacc6835a1b5d0c61cfae08421
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:45:34 -0300

    Added Abyssal Duality to FreezeDry

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/abyss/FreezeDryDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/abyss/FreezeDryDual.java
@@ -1,31 +1,28 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.abyss;
 
 import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.FreezeDry;
 import ShadowSiren.cards.abstractCards.AbstractAbyssCard;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
-import ShadowSiren.cards.dualityCards.abyss.FreezeDryDual;
-import ShadowSiren.cards.interfaces.FistAttack;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.DrenchPower;
 import ShadowSiren.powers.FreezePower;
-import ShadowSiren.stances.HugeStance;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.FrostOrbActivateEffect;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class FreezeDry extends AbstractAbyssCard {
+public class FreezeDryDual extends AbstractAbyssCard implements UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(FreezeDry.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(FreezeDryDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderAttack.png");
 
     // /TEXT DECLARATION/
@@ -46,8 +43,8 @@ public class FreezeDry extends AbstractAbyssCard {
     // /STAT DECLARATION/
 
 
-    public FreezeDry() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new FreezeDryDual());
+    public FreezeDryDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         damage = baseDamage = DAMAGE;
         magicNumber = baseMagicNumber = FREEZE;
     }
@@ -55,8 +52,22 @@ public class FreezeDry extends AbstractAbyssCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m.hasPower(DrenchPower.POWER_ID)) {
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
+        }
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         this.addToBot(new ApplyPowerAction(m, p, new FreezePower(m, magicNumber)));
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        super.triggerOnGlowCheck();
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if (!m.isDeadOrEscaped() && m.hasPower(DrenchPower.POWER_ID)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
     }
 
     //Upgraded stats.

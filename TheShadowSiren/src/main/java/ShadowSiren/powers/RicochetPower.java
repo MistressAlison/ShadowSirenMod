a1a69be557c430476f736835cff290956d7a6b55
MistressAlison <alisonmoons@gmail.com>
2021-05-09 10:47:54 -0300

    Added power for Ferment Huge Form

---
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/RicochetPower.java
@@ -0,0 +1,94 @@
+package ShadowSiren.powers;
+
+import ShadowSiren.ShadowSirenMod;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class RicochetPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = ShadowSirenMod.makeID("RicochetPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private boolean justEvoked = true;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public RicochetPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        this.loadRegion("rebound");
+        this.isTurnBased = true;
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    public void onAfterUseCard(AbstractCard card, UseCardAction action) {
+        if (this.justEvoked) {
+            this.justEvoked = false;
+        } else {
+            //Don't waste a use if the card already was going to return
+            if (!card.returnToHand) {
+                if (card.type != AbstractCard.CardType.POWER) {
+                    this.flash();
+                    //action.returnToHand = true;
+                    card.returnToHand = true;
+                    this.addToTop(new AbstractGameAction() {
+                        @Override
+                        public void update() {
+                            card.returnToHand = false;
+                            this.isDone = true;
+                        }
+                    });
+                }
+                if (this.amount == 1) {
+                    this.addToBot(new ReducePowerAction(owner, owner, this, 1));
+                } else {
+                    this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+                }
+            }
+        }
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer) {
+            this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+        }
+
+    }
+
+    @Override
+    public void updateDescription() {
+        if (this.amount > 1) {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        } else {
+            this.description = DESCRIPTIONS[0];
+        }
+
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new RicochetPower(owner, amount);
+    }
+}

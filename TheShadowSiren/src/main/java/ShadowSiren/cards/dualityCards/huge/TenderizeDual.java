38d71d8d5fb211496a772a17bcbad5e17be27efc
MistressAlison <alisonmoons@gmail.com>
2021-05-12 13:39:35 -0300

    Added Huge Duality to Tenderize

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/TenderizeDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/TenderizeDual.java
@@ -1,25 +1,24 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.huge;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.cards.abstractCards.AbstractHugeCard;
-import ShadowSiren.cards.dualityCards.huge.TenderizeDual;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.SoftPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Tenderize extends AbstractHugeCard {
+public class TenderizeDual extends AbstractHugeCard implements UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(Tenderize.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(TenderizeDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderAttack.png");
 
     // /TEXT DECLARATION/
@@ -36,23 +35,23 @@ public class Tenderize extends AbstractHugeCard {
     private static final int DAMAGE = 6;
     private static final int UPGRADE_PLUS_DMG = 2;
 
-    private static final int SOFT = 1;
-    private static final int UPGRADE_PLUS_SOFT = 1;
+    private static final int VULN = 1;
+    private static final int UPGRADE_PLUS_VULN = 1;
 
     // /STAT DECLARATION/
 
 
-    public Tenderize() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new TenderizeDual());
+    public TenderizeDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         damage = baseDamage = DAMAGE;
-        magicNumber = baseMagicNumber = SOFT;
+        magicNumber = baseMagicNumber = VULN;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
-        this.addToBot(new ApplyPowerAction(m, p, new SoftPower(m, magicNumber)));
+        this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
     }
 
     //Upgraded stats.
@@ -61,7 +60,7 @@ public class Tenderize extends AbstractHugeCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_SOFT);
+            upgradeMagicNumber(UPGRADE_PLUS_VULN);
             initializeDescription();
             super.upgrade();
         }

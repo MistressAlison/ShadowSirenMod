c202222a497c53fdbebd28809b7fcd8f31ad8986
MistressAlison <alisonmoons@gmail.com>
2021-05-12 13:45:44 -0300

    Added Smoke Duality to Fiery Jinx

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/smoke/FieryJinxDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/smoke/FieryJinxDual.java
@@ -1,25 +1,25 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.smoke;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.actions.TriggerBurnAction;
+import ShadowSiren.cards.FieryJinx;
 import ShadowSiren.cards.abstractCards.AbstractSmokeCard;
-import ShadowSiren.cards.dualityCards.smoke.FieryJinxDual;
 import ShadowSiren.cards.interfaces.VigorMagicBuff;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.BurnPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class FieryJinx extends AbstractSmokeCard implements VigorMagicBuff {
+public class FieryJinxDual extends AbstractSmokeCard implements VigorMagicBuff, UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(FieryJinx.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(FieryJinxDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderSkill.png");
 
     // /TEXT DECLARATION/
@@ -37,12 +37,16 @@ public class FieryJinx extends AbstractSmokeCard implements VigorMagicBuff {
     private static final int BURN = 3;
     private static final int UPGRADE_PLUS_BURN = 1;
 
+    private static final int TRIGGER = 1;
+    private static final int UPGRADE_PLUS_TRIGGER = 1;
+
     // /STAT DECLARATION/
 
 
-    public FieryJinx() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new FieryJinxDual());
+    public FieryJinxDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = BURN;
+        secondMagicNumber = baseSecondMagicNumber = TRIGGER;
     }
 
     // Actions the card should do.
@@ -51,6 +55,7 @@ public class FieryJinx extends AbstractSmokeCard implements VigorMagicBuff {
         for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
             if(!aM.isDeadOrEscaped()) {
                 this.addToBot(new ApplyPowerAction(aM, p, new BurnPower(aM, p, magicNumber)));
+                this.addToBot(new TriggerBurnAction(aM, secondMagicNumber));
             }
         }
     }
@@ -61,6 +66,7 @@ public class FieryJinx extends AbstractSmokeCard implements VigorMagicBuff {
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_BURN);
+            upgradeSecondMagicNumber(UPGRADE_PLUS_TRIGGER);
             initializeDescription();
             super.upgrade();
         }

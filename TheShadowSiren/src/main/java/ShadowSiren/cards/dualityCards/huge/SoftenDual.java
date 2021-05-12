d6a52dee20c1c7da5b47ce3f2c17dfe4e614a030
MistressAlison <alisonmoons@gmail.com>
2021-05-12 13:41:14 -0300

    Added Huge Duality to Soften

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/SoftenDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/huge/SoftenDual.java
@@ -1,22 +1,22 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.huge;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.cards.abstractCards.AbstractHugeCard;
-import ShadowSiren.cards.dualityCards.huge.SoftenDual;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.SoftPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Soften extends AbstractHugeCard {
+public class SoftenDual extends AbstractHugeCard implements UniqueCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(Soften.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(SoftenDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderSkill.png");
 
     // /TEXT DECLARATION/
@@ -29,31 +29,24 @@ public class Soften extends AbstractHugeCard {
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
-    private static final int COST = 1;
-    private static final int UPGRADE_COST = 0;
+    private static final int COST = 0;
 
-    private static final int SOFT = 3;
+    private static final int EFFECT = 1;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
 
     // /STAT DECLARATION/
 
 
-    public Soften() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new SoftenDual());
-        magicNumber = baseMagicNumber = SOFT;
+    public SoftenDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        /*if (upgraded) {
-            for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
-                if (!aM.isDeadOrEscaped()) {
-                    this.addToBot(new ApplyPowerAction(aM, p, new SoftPower(aM, magicNumber)));
-                }
-            }
-        } else { */
-            this.addToBot(new ApplyPowerAction(m, p, new SoftPower(m, magicNumber)));
-        //}
+        this.addToBot(new ApplyPowerAction(m, p, new SoftPower(m, magicNumber)));
+        this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
     }
 
     //Upgraded stats.
@@ -61,9 +54,7 @@ public class Soften extends AbstractHugeCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeBaseCost(UPGRADE_COST);
-            //rawDescription = UPGRADE_DESCRIPTION;
-            //target = CardTarget.ALL_ENEMY;
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
             super.upgrade();
         }

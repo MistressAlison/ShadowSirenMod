26e5e4788b14313b5e8a1494dd6281eebef43001
MistressAlison <alisonmoons@gmail.com>
2021-05-12 13:44:49 -0300

    Renamed Blast Burn to Incinerate. Added Smoke Duality named Blast Burn

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Incinerate.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Incinerate.java
@@ -2,18 +2,21 @@ package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.actions.BlastBurnAction;
+import ShadowSiren.actions.NerfedBlastBurnAction;
 import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cards.abstractCards.AbstractSmokeCard;
+import ShadowSiren.cards.dualityCards.smoke.IncinerateDual;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class BlastBurn extends AbstractDynamicCard {
+public class Incinerate extends AbstractSmokeCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(BlastBurn.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(Incinerate.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderAttack.png");
 
     // /TEXT DECLARATION/
@@ -26,16 +29,16 @@ public class BlastBurn extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
-    private static final int COST = 4;
-    private static final int DAMAGE = 20;
+    private static final int COST = 2;
+    private static final int DAMAGE = 10;
     private static final int UPGRADE_PLUS_DMG = 5;
 
 
     // /STAT DECLARATION/
 
 
-    public BlastBurn() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    public Incinerate() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new IncinerateDual());
         damage = baseDamage = DAMAGE;
         isMultiDamage = true;
     }
@@ -43,7 +46,7 @@ public class BlastBurn extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new BlastBurnAction(p, multiDamage, damageTypeForTurn));
+        this.addToBot(new NerfedBlastBurnAction(p, multiDamage, damageTypeForTurn));
     }
 
     //Upgraded stats.
@@ -53,6 +56,7 @@ public class BlastBurn extends AbstractDynamicCard {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
             initializeDescription();
+            super.upgrade();
         }
     }
 }

2f5ca2d5164b518402f93ac0552fa3cb90f2d80e
MistressAlison <alisonmoons@gmail.com>
2021-05-12 13:42:31 -0300

    Added Hyper Duality to Preemptive Strike

--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/hyper/PreemptiveStrikeDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/hyper/PreemptiveStrikeDual.java
@@ -1,11 +1,14 @@
-package ShadowSiren.cards;
+package ShadowSiren.cards.dualityCards.hyper;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cards.PreemptiveStrike;
 import ShadowSiren.cards.abstractCards.AbstractHyperCard;
-import ShadowSiren.cards.dualityCards.hyper.PreemptiveStrikeDual;
+import ShadowSiren.cards.interfaces.ChargeMagicBuff;
+import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ElectricPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -15,11 +18,11 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class PreemptiveStrike extends AbstractHyperCard {
+public class PreemptiveStrikeDual extends AbstractHyperCard implements UniqueCard, ChargeMagicBuff {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(PreemptiveStrike.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(PreemptiveStrikeDual.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderAttack.png");
 
     // /TEXT DECLARATION/
@@ -37,12 +40,15 @@ public class PreemptiveStrike extends AbstractHyperCard {
     private static final int DAMAGE = 8;
     private static final int UPGRADE_PLUS_DAMAGE = 2;
 
+    private static final int ELEC = 1;
+
     // /STAT DECLARATION/
 
 
-    public PreemptiveStrike() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new PreemptiveStrikeDual());
+    public PreemptiveStrikeDual() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         damage = baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = ELEC;
         isInnate = true;
         isEthereal = true;
         //exhaust = true;
@@ -54,20 +60,10 @@ public class PreemptiveStrike extends AbstractHyperCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         //Once we play this card, it will recalculate the damage AFTER adding this to the cardPlayed array, so we need a different check
         if (AbstractDungeon.actionManager.cardsPlayedThisTurn.size() == 1) {
-            damage *= 2;
+            this.addToBot(new ApplyPowerAction(p, p, new ElectricPower(p, magicNumber)));
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
         }
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-    }
-
-    @Override
-    public void calculateCardDamage(AbstractMonster mo) {
-        super.calculateCardDamage(mo);
-        //These checks fail when we actually play the card and are only here to show the updated damage value when no card has been played
-        if (AbstractDungeon.actionManager.cardsPlayedThisTurn.size() == 0) {
-            damage *= 2;
-            isDamageModified = damage != baseDamage;
-            initializeDescription();
-        }
     }
 
     @Override
@@ -84,8 +80,6 @@ public class PreemptiveStrike extends AbstractHyperCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DAMAGE);
-            //exhaust = false;
-            //rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
             super.upgrade();
         }

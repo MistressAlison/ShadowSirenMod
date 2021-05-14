8df1f82d63019cc4f55f6e557be09b799415beaa
MistressAlison <alisonmoons@gmail.com>
2021-05-14 01:25:16 -0300

    EE renamed and reworked to Black Magic. Now increases the amount of debuffs by 1 when they are applied.

--- TheShadowSiren/src/main/java/ShadowSiren/cards/BlackMagic.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlackMagic.java
@@ -3,19 +3,18 @@ package ShadowSiren.cards;
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.ElementalEquilibriumPower;
-import ShadowSiren.powers.ReinforcedPlatingPower;
+import ShadowSiren.powers.BlackMagicPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class ElementalEquilibrium extends AbstractDynamicCard {
+public class BlackMagic extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(ElementalEquilibrium.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(BlackMagic.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderPower.png");
 
     // /TEXT DECLARATION/
@@ -28,15 +27,15 @@ public class ElementalEquilibrium extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
-    private static final int COST = 2;
-    private static final int UPGRADE_COST = 1;
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
 
     private static final int EFFECT = 1;
 
     // /STAT DECLARATION/
 
 
-    public ElementalEquilibrium() {
+    public BlackMagic() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         magicNumber = baseMagicNumber = EFFECT;
     }
@@ -44,7 +43,7 @@ public class ElementalEquilibrium extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new ElementalEquilibriumPower(p, magicNumber)));
+        this.addToBot(new ApplyPowerAction(p, p, new BlackMagicPower(p, magicNumber)));
     }
 
     //Upgraded stats.

50aeca268a4d23307d381d1d8300d0ff93fdad84
MistressAlison <alisonmoons@gmail.com>
2021-05-23 21:27:58 -0300

    Added Block to Dark Embrace and its dual

--- TheShadowSiren/src/main/java/ShadowSiren/cards/DarkEmbrace.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/DarkEmbrace.java
@@ -6,6 +6,7 @@ import ShadowSiren.cards.dualityCards.veil.DarkEmbraceDual;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -25,13 +26,15 @@ public class DarkEmbrace extends AbstractShadowCard {
     // STAT DECLARATION
 
     private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
-    private static final AbstractCard.CardTarget TARGET = CardTarget.ENEMY;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
     private static final AbstractCard.CardType TYPE = CardType.ATTACK;
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int DAMAGE = 7;
+    private static final int DAMAGE = 5;
     private static final int UPGRADE_PLUS_DAMAGE = 2;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
 
     // /STAT DECLARATION/
 
@@ -39,12 +42,14 @@ public class DarkEmbrace extends AbstractShadowCard {
     public DarkEmbrace() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new DarkEmbraceDual());
         damage = baseDamage = DAMAGE;
+        block = baseBlock = BLOCK;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        this.addToBot(new GainBlockAction(p, p, block));
     }
 
     //Upgraded stats.
@@ -53,6 +58,7 @@ public class DarkEmbrace extends AbstractShadowCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
             initializeDescription();
             super.upgrade();
         }
--- TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/veil/DarkEmbraceDual.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/dualityCards/veil/DarkEmbraceDual.java
@@ -6,6 +6,7 @@ import ShadowSiren.cards.uniqueCards.UniqueCard;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -25,13 +26,15 @@ public class DarkEmbraceDual extends AbstractShadowCard implements UniqueCard {
     // STAT DECLARATION
 
     private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
-    private static final AbstractCard.CardTarget TARGET = CardTarget.ENEMY;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
     private static final AbstractCard.CardType TYPE = CardType.ATTACK;
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
     private static final int DAMAGE = 7;
     private static final int UPGRADE_PLUS_DAMAGE = 2;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
 
     // /STAT DECLARATION/
 
@@ -39,6 +42,7 @@ public class DarkEmbraceDual extends AbstractShadowCard implements UniqueCard {
     public DarkEmbraceDual() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         damage = baseDamage = DAMAGE;
+        block = baseBlock = BLOCK;
     }
 
     // Actions the card should do.
@@ -46,6 +50,7 @@ public class DarkEmbraceDual extends AbstractShadowCard implements UniqueCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_VERTICAL, true));
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        this.addToBot(new GainBlockAction(p, p, block));
     }
 
     //Upgraded stats.
@@ -54,6 +59,7 @@ public class DarkEmbraceDual extends AbstractShadowCard implements UniqueCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
             initializeDescription();
             super.upgrade();
         }

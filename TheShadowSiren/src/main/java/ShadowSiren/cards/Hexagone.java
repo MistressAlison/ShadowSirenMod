6d3a4272cb7460ba163b4969466a658090c84019
MistressAlison <alisonmoons@gmail.com>
2021-05-23 21:27:20 -0300

    Hexagone now triggers Hexing, rather than adding Burn

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Hexagone.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Hexagone.java
@@ -3,14 +3,17 @@ package ShadowSiren.cards;
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.DrenchPower;
 import ShadowSiren.powers.HexingPower;
 import ShadowSiren.powers.BurnPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
@@ -51,10 +54,21 @@ public class Hexagone extends AbstractDynamicCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
         if (m.hasPower(HexingPower.POWER_ID)) {
-            int amount = m.getPower(HexingPower.POWER_ID).amount;
-            amount *= magicNumber;
-            this.addToBot(new ApplyPowerAction(m, p, new BurnPower(m, p, amount)));
+            for (int i = 0 ; i < magicNumber ; i++) {
+                ((HexingPower)m.getPower(HexingPower.POWER_ID)).triggerEffect();
+            }
             this.addToBot(new RemoveSpecificPowerAction(m, p, m.getPower(HexingPower.POWER_ID)));
+        }
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if (!m.isDeadOrEscaped() && m.hasPower(HexingPower.POWER_ID)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
         }
     }
 

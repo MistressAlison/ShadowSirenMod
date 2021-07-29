124d550aceb90de6a2e5a765ebe54f744365a457
MistressAlison <alisonmoons@gmail.com>
2021-07-29 14:37:05 -0300

    Added Frozen Shield using new Ice Block

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/FrozenShield.java
@@ -0,0 +1,58 @@
+package ShadowSiren.cards;
+
+import IconsAddon.actions.GainCustomBlockAction;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.characters.Vivian;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class FrozenShield extends AbstractIceCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(FrozenShield.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+
+    // /STAT DECLARATION/
+
+    public FrozenShield() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainCustomBlockAction(new IceBlock(p, block)));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+}

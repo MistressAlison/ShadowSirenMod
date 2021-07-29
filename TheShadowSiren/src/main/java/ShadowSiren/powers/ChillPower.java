1ceba4789ab1734b73d9513859d7687cfbdbba98
MistressAlison <alisonmoons@gmail.com>
2021-07-29 14:25:14 -0300

    Removed Freeze from Chill. May reimplement later

--- TheShadowSiren/src/main/java/ShadowSiren/powers/ChillPower.java
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/ChillPower.java
@@ -4,9 +4,14 @@ import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.relics.DataCollector;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.blue.Chill;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
@@ -21,19 +26,25 @@ public class ChillPower extends AbstractPower implements CloneablePowerInterface
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
+    public static final int REDUCTION_PERCENT = 1;
+    public static final int ROLLOVER_STACK = 50;
+    private static final float DECAY_RATE = 1/3f;
+
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
     //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
 
-    public ChillPower(AbstractMonster owner, int amount) {
+    //TODO its OP, lol
+    public ChillPower(AbstractCreature owner, int amount) {
         this.name = NAME;
         this.ID = POWER_ID;
         this.owner = owner;
         this.amount = amount;
 
         this.type = PowerType.DEBUFF;
-        this.isTurnBased = false;
+        this.isTurnBased = true;
+        this.priority = 999;
 
         // We load those txtures here.
         this.loadRegion("skillBurn");
@@ -44,66 +55,83 @@ public class ChillPower extends AbstractPower implements CloneablePowerInterface
     }
 
     @Override
-    public void onInitialApplication() {
-        super.onInitialApplication();
-        CardCrawlGame.sound.play("ORB_FROST_EVOKE", 0.1F);
+    public void playApplyPowerSfx() {
+        this.addToTop(new SFXAction("ORB_FROST_EVOKE", 0.1F));
         AbstractDungeon.effectsQueue.add(new FrostOrbActivateEffect(owner.hb_x, owner.hb_y));
         owner.tint.color = Color.BLUE.cpy();
         owner.tint.changeColor(Color.WHITE.cpy());
+    }
+
+    @Override
+    public void onInitialApplication() {
+        super.onInitialApplication();
         if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
             DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
             dataCollector.onApplyChill(amount);
         }
-        if (owner.hasPower(DrenchPower.POWER_ID)) {
-            owner.getPower(DrenchPower.POWER_ID).flash();
-            this.addToTop(new RemoveSpecificPowerAction(owner, owner, DrenchPower.POWER_ID));
-            this.addToBot(new ApplyPowerAction(owner, owner, new ChillPower((AbstractMonster) owner, owner.getPower(DrenchPower.POWER_ID).amount*DrenchPower.CHILL_BONUS)));
-        }
-        if (amount >= 10) {
-            int effect = amount / 10;
-            this.addToBot(new ReducePowerAction(owner, owner, this, effect*10));
-            this.addToBot(new ApplyPowerAction(owner, owner, new FreezePower((AbstractMonster) owner, effect)));
-            if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
-                DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-                dataCollector.onChillRollover(effect);
-            }
-        }
+        //freezeCheck();
     }
 
     @Override
     public void stackPower(int stackAmount) {
         super.stackPower(stackAmount);
-        CardCrawlGame.sound.play("ORB_FROST_EVOKE", 0.1F);
-        AbstractDungeon.effectsQueue.add(new FrostOrbActivateEffect(owner.hb_x, owner.hb_y));
-        owner.tint.color = Color.BLUE.cpy();
-        owner.tint.changeColor(Color.WHITE.cpy());
         if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
             DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
             dataCollector.onApplyChill(amount);
         }
-        if (owner.hasPower(DrenchPower.POWER_ID)) {
-            owner.getPower(DrenchPower.POWER_ID).flash();
-            this.addToTop(new RemoveSpecificPowerAction(owner, owner, DrenchPower.POWER_ID));
-            this.addToBot(new ApplyPowerAction(owner, owner, new ChillPower((AbstractMonster) owner, owner.getPower(DrenchPower.POWER_ID).amount*DrenchPower.CHILL_BONUS)));
-        }
-        if (amount >= 10) {
-            int effect = amount / 10;
-            this.addToBot(new ReducePowerAction(owner, owner, this, effect*10));
-            this.addToBot(new ApplyPowerAction(owner, owner, new FreezePower((AbstractMonster) owner, effect)));
-            if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
-                DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-                dataCollector.onChillRollover(effect);
+        //freezeCheck();
+    }
+
+    public void freezeCheck() {
+        this.addToTop(new AbstractGameAction() {
+            @Override
+            public void update() {
+                if (owner.getPower(ChillPower.POWER_ID).amount >= ROLLOVER_STACK) {
+                    int effect = amount / ROLLOVER_STACK;
+                    this.addToTop(new ReducePowerAction(owner, owner, ChillPower.this, effect*ROLLOVER_STACK));
+                    this.addToTop(new ApplyPowerAction(owner, owner, new FreezePower((AbstractMonster) owner, effect)));
+                    if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
+                        DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
+                        dataCollector.onChillRollover(effect);
+                    }
+                }
+                this.isDone = true;
             }
+        });
+    }
+
+    @Override
+    public float atDamageGive(float damage, DamageInfo.DamageType type) {
+        if (type == DamageInfo.DamageType.NORMAL) {
+            float multiplier = REDUCTION_PERCENT/100f;
+            return Math.max(0, damage * (1 - (multiplier * this.amount)));
+        } else {
+            return damage;
         }
+    }
+
+    @Override
+    public void atEndOfRound() {
+        this.flashWithoutSound();
+        if (this.amount <= 1) {
+            this.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
+        } else {
+            int reduce = Math.max(1, (int)(this.amount * DECAY_RATE));
+            this.addToTop(new ReducePowerAction(owner, owner, this, reduce));
+            updateDescription();
+        }
+        //if (!attackedThisTurn) {
+        //}
+        //attackedThisTurn = false;
     }
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0];
+        description = DESCRIPTIONS[0] + (REDUCTION_PERCENT*amount) + DESCRIPTIONS[1];
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new ChillPower((AbstractMonster) owner, amount);
+        return new ChillPower(owner, amount);
     }
 }

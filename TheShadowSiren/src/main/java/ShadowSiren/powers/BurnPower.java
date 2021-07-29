90940edc2db12acb23bc4fc1c7f2295481d4178a
MistressAlison <alisonmoons@gmail.com>
2021-07-29 14:21:19 -0300

    Burn now decays slowly

--- TheShadowSiren/src/main/java/ShadowSiren/powers/BurnPower.java
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/BurnPower.java
@@ -4,6 +4,7 @@ import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.actions.BurnToAshEffect;
 import ShadowSiren.powers.interfaces.OnRemoveOtherPowerPower;
 import ShadowSiren.relics.DataCollector;
+import basemod.ReflectionHacks;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,29 +19,32 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 
-public class BurnPower extends AbstractPower implements CloneablePowerInterface, HealthBarRenderPower, OnRemoveOtherPowerPower, OnReceivePowerPower {
+public class BurnPower extends AbstractPower implements CloneablePowerInterface, HealthBarRenderPower {
 
     public static final String POWER_ID = ShadowSirenMod.makeID("BurnPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
+    private final AbstractCreature source;
+    private static final float DECAY_RATE = 1/3f;
+    private static final int DETONATE_MULTIPLIER = 3;
     private final Color hpBarColor = new Color(-2686721);
 
-    private final AbstractCreature source;
-    private int damagePerTurn;
-    //public boolean activated;
+    //private boolean attackedThisTurn;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
     //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
     //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
-
 
     public BurnPower(AbstractCreature owner, AbstractCreature source, int amount) {
         this.name = NAME;
@@ -48,7 +52,7 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
         this.owner = owner;
         this.amount = amount;
         this.source = source;
-        this.priority = 0;
+
         this.type = PowerType.DEBUFF;
         this.isTurnBased = true;
 
@@ -64,27 +68,24 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
     @Override
     public void onInitialApplication() {
         super.onInitialApplication();
-        tallyDebuffs();
-        drenchInteraction();
+        //drenchInteraction();
         if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
             DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
             dataCollector.onApplyBurn(this.amount);
         }
-        //deathCheck(0);
     }
 
     @Override
     public void stackPower(int stackAmount) {
         super.stackPower(stackAmount);
-        tallyDebuffs();
-        drenchInteraction();
+        //drenchInteraction();
         if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
             DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
             dataCollector.onApplyBurn(stackAmount);
         }
-        //deathCheck(0);
     }
 
+    /*
     private void drenchInteraction() {
         if (owner.hasPower(DrenchPower.POWER_ID)) {
             DrenchPower drench = (DrenchPower) owner.getPower(DrenchPower.POWER_ID);
@@ -92,12 +93,12 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
             int limit = Math.min(drench.amount, this.amount);
             this.flashWithoutSound();
             drench.flashWithoutSound();
-            for (int i = 0 ; i < limit ; i++) {
+            for (int i = 0; i < limit; i++) {
                 if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
                     DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-                    dataCollector.onBurnDamage(damagePerTurn);
+                    dataCollector.onBurnDamage(amount);
                 }
-                this.addToTop(new DamageAction(owner, new DamageInfo(source, damagePerTurn, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.FIRE, true));
+                this.addToTop(new DamageAction(owner, new DamageInfo(source, amount, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.FIRE, true));
             }
             if (equal) {
                 this.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
@@ -112,61 +113,43 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
                     updateDescription();
                 }
             }
-            //Old effect
-            /*AbstractDungeon.effectList.add(new FlashAtkImgEffect(owner.hb.cX, owner.hb.cY, AbstractGameAction.AttackEffect.FIRE));
-            AbstractDungeon.effectList.add(new ExplosionSmallEffect(owner.hb.cX, owner.hb.cY));
-            this.addToTop(new RemoveSpecificPowerAction(owner, owner, DrenchPower.POWER_ID));
-            this.addToBot(new DamageAllEnemiesAction(source, DamageInfo.createDamageMatrix(owner.getPower(DrenchPower.POWER_ID).amount*DrenchPower.STEAM_DAMAGE, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE, true));
-            for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
-                if (!m.isDeadOrEscaped()) {
-                    this.addToBot(new ApplyPowerAction(m, source, new BurnPower(m, source, owner.getPower(DrenchPower.POWER_ID).amount*DrenchPower.STEAM_BURN), owner.getPower(DrenchPower.POWER_ID).amount*DrenchPower.STEAM_BURN, true));
-                }
-            }*/
         }
-    }
-
-    @Override
-    public void atEndOfRound() {
-        triggerEffect();
-    }
-
-    public void triggerEffect() {
-        this.flashWithoutSound();
-        if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
-            DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-            dataCollector.onBurnDamage(damagePerTurn);
-        }
-        this.addToBot(new DamageAction(owner, new DamageInfo(source, damagePerTurn, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.FIRE, true));
-        if (this.amount == 1) {
-            this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
-        } else {
-            this.addToBot(new ReducePowerAction(owner, owner, this, 1));
-        }
-        updateDescription();
-    }
+    }*/
 
     public void detonate() {
         this.flashWithoutSound();
         if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
             DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-            dataCollector.onBurnDamage(damagePerTurn*amount);
+            dataCollector.onBurnDamage(amount*2);
         }
         this.addToBot(new VFXAction(new ExplosionSmallEffect(owner.hb.cX, owner.hb.cY), 0.1F));
         this.addToBot(new SFXAction("GHOST_ORB_IGNITE_2", 0.3F));
         this.addToBot(new SFXAction("GHOST_ORB_IGNITE_1", 0.3F));
-        this.addToBot(new DamageAction(owner, new DamageInfo(source, damagePerTurn*amount, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.NONE, true));
+        for (int i = 0 ; i < DETONATE_MULTIPLIER ; i++) {
+            triggerEffect();
+        }
         this.addToBot(new RemoveSpecificPowerAction(owner, owner, this));
     }
 
     @Override
-    public void updateDescription() {
-        description = DESCRIPTIONS[0] + damagePerTurn + DESCRIPTIONS[1];
+    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target) {
+        triggerEffect();
     }
 
-    @Override
+    public void triggerEffect() {
+        if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && !AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            this.flash();
+            this.addToTop(new DamageAction(owner, new DamageInfo(source, amount, DamageInfo.DamageType.HP_LOSS), AbstractGameAction.AttackEffect.NONE, true));
+            if (AbstractDungeon.player.hasRelic(DataCollector.ID) && source == AbstractDungeon.player) {
+                DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
+                dataCollector.onBurnDamage(amount);
+            }
+            //attackedThisTurn = true;
+        }
+    }
+
+    /*@Override
     public boolean onReceivePower(AbstractPower abstractPower, AbstractCreature target, AbstractCreature source) {
-        //We have to use a delayed tally here since the power hasnt actually been added yet
-        delayedTallyDebuffs();
         //If the new debuff is Drench, we need to set a delayed interaction (so it actually has time to apply)
         if (abstractPower instanceof DrenchPower) {
             this.addToTop(new AbstractGameAction() {
@@ -178,40 +161,26 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
             });
         }
         return true;
+    }*/
+
+    @Override
+    public void atEndOfRound() {
+        this.flashWithoutSound();
+        if (this.amount <= 1) {
+            this.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
+        } else {
+            int reduce = Math.max(1, (int)(this.amount * DECAY_RATE));
+            this.addToTop(new ReducePowerAction(owner, owner, this, reduce));
+            updateDescription();
+        }
+        //if (!attackedThisTurn) {
+        //}
+        //attackedThisTurn = false;
     }
 
     @Override
-    public void onRemoveOtherPower(AbstractPower powerRemoved) {
-        //We can tally right away as this patch takes place directly after the power is removed
-        tallyDebuffs();
-    }
-
-    public void tallyDebuffs() {
-        damagePerTurn = 0;
-        for (AbstractPower pow : owner.powers) {
-            if (pow.type == PowerType.DEBUFF) {
-                damagePerTurn++;
-                if (pow instanceof FoxFirePower && pow.amount > 1) {
-                    damagePerTurn += (pow.amount - 1);
-                }
-            }
-        }
-        updateDescription();
-    }
-
-    public void delayedTallyDebuffs() {
-        this.addToTop(new AbstractGameAction() {
-            @Override
-            public void update() {
-                tallyDebuffs();
-                this.isDone = true;
-            }
-        });
-    }
-
-    public void renderAmount(SpriteBatch sb, float x, float y, Color c) {
-        FontHelper.renderFontRightTopAligned(sb, FontHelper.powerAmountFont, Integer.toString(this.amount), x, y, this.fontScale, c);
-        FontHelper.renderFontRightTopAligned(sb, FontHelper.powerAmountFont, Integer.toString(damagePerTurn), x, y + 15.0F * Settings.scale, this.fontScale, c);
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
     }
 
     @Override
@@ -221,7 +190,17 @@ public class BurnPower extends AbstractPower implements CloneablePowerInterface,
 
     @Override
     public int getHealthBarAmount() {
-        return damagePerTurn;
+        int amt = amount;
+        if (owner instanceof AbstractMonster) {
+            if (((AbstractMonster) owner).getIntentBaseDmg() >= 0) {
+                if (ReflectionHacks.getPrivate(owner, AbstractMonster.class, "isMultiDmg")) {
+                    amt *= ReflectionHacks.<Integer>getPrivate(owner, AbstractMonster.class, "intentMultiAmt");
+                }
+            } else {
+                amt = 0;
+            }
+        }
+        return amt;
     }
 
     @Override

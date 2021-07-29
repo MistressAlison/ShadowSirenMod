2c7122bb0cc1a0b7100922d0d5f35ac6fe156555
MistressAlison <alisonmoons@gmail.com>
2021-07-29 14:36:29 -0300

    Made new Shade Fist, now a 0 cost Fire card

--- TheShadowSiren/src/main/java/ShadowSiren/cards/ShadeFist.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ShadeFist.java
@@ -1,73 +1,59 @@
 package ShadowSiren.cards;
 
+import IconsAddon.util.DamageModifierHelper;
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
-import ShadowSiren.cards.abstractCards.AbstractHugeCard;
-import ShadowSiren.cards.dualityCards.huge.ShadeFistDual;
-import ShadowSiren.cards.interfaces.FistAttack;
-import ShadowSiren.cards.interfaces.VigorMagicBuff;
+import ShadowSiren.cards.abstractCards.AbstractFireCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.BurnPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class ShadeFist extends AbstractHugeCard implements FistAttack, VigorMagicBuff {
+public class ShadeFist extends AbstractFireCard {
+
 
     // TEXT DECLARATION
 
     public static final String ID = ShadowSirenMod.makeID(ShadeFist.class.getSimpleName());
     public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
 
     // /TEXT DECLARATION/
 
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardRarity RARITY = CardRarity.BASIC;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
-    private static final int COST = 1;
-    private static final int DAMAGE = 5;
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 2;
-
-    private static final int BURN = 4;
-    private static final int UPGRADE_PLUS_BURN = 1;
 
     // /STAT DECLARATION/
 
-
     public ShadeFist() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new ShadeFistDual());
-        damage = baseDamage = DAMAGE;
-        magicNumber = baseMagicNumber = BURN;
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
-        this.addToBot(new ApplyPowerAction(m, p, new BurnPower(m, p, magicNumber)));
+        this.addToBot(new DamageAction(m, DamageModifierHelper.makeBoundDamageInfo(this, p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 
-    //Upgraded stats.
+    // Upgraded stats.
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_BURN);
             initializeDescription();
-            super.upgrade();
         }
     }
 }

ce9167ba324f3d768f827f21564bbff009b384e3
MistressAlison <alisonmoons@gmail.com>
2021-07-29 14:46:11 -0300

    Added Chain Shock to try out new Electric Damage on an AOE. Its kinda OP

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ChainShock.java
@@ -0,0 +1,85 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.DamageModifierHelper;
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cards.abstractCards.AbstractElectricCard;
+import ShadowSiren.cards.interfaces.MagicAnimation;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.ElectricDamage;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.LightningOrbActivateEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class ChainShock extends AbstractElectricCard implements MagicAnimation {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(ChainShock.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DMG = 2;
+
+    // /STAT DECLARATION/
+
+    public ChainShock() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        isMultiDamage = true;
+        this.exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                for (AbstractPower p : AbstractDungeon.player.powers) {
+                    p.onDamageAllEnemies(multiDamage);
+                }
+                for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+                    CardCrawlGame.sound.play("ORB_LIGHTNING_CHANNEL", 0.2f);
+                    AbstractDungeon.effectList.add(new LightningOrbActivateEffect(aM.hb.cX, aM.hb.cY));
+                    aM.damage(DamageModifierHelper.makeBoundDamageInfo(this, p, multiDamage[AbstractDungeon.getMonsters().monsters.indexOf(aM)], damageTypeForTurn));
+                }
+                if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                    AbstractDungeon.actionManager.clearPostCombatActions();
+                }
+                this.isDone = true;
+            }
+        });
+
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}

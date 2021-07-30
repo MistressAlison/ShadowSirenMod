047f43614f6fdf2e9af6704ab3f49e1d9f44a628
MistressAlison <alisonmoons@gmail.com>
2021-07-30 15:55:14 -0300

    Added Energizing Flush - Removes active Elements then gives energy for each removed

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/EnergizingFlush.java
@@ -0,0 +1,69 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ElementalPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.EmptyStanceEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class EnergizingFlush extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(EnergizingFlush.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+
+    // /STAT DECLARATION/
+
+
+    public EnergizingFlush() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int effect = ElementalPower.numActiveElements();
+        if (effect > 0) {
+            this.addToBot(new VFXAction(new EmptyStanceEffect(p.hb.cX, p.hb.cY)));
+            this.addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    ElementalPower.removeAllElements();
+                    this.isDone = true;
+                }
+            });
+            this.addToBot(new GainEnergyAction(effect));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            this.selfRetain = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}

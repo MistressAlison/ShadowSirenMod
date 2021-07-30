df59f7a115125ace622fd84010dc2eb467bab019
MistressAlison <alisonmoons@gmail.com>
2021-07-30 15:54:47 -0300

    Added Shield Conversion - Removes active Elements then gives Block for each removed

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ShieldConversion.java
@@ -0,0 +1,71 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ElementalPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.EmptyStanceEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class ShieldConversion extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(ShieldConversion.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 6;
+    private static final int UPGRADE_PLUS_BLOCK = 3;
+
+    // /STAT DECLARATION/
+
+
+    public ShieldConversion() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int effect = ElementalPower.numActiveElements();
+        if (effect > 0) {
+            this.addToBot(new VFXAction(new EmptyStanceEffect(p.hb.cX, p.hb.cY)));
+            this.addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    ElementalPower.removeAllElements();
+                    this.isDone = true;
+                }
+            });
+            this.addToBot(new GainBlockAction(p, p, effect*block));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+}

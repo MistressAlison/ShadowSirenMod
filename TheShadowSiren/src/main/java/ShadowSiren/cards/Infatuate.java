6cad57aef5402e081f4a1661cac1ccff013e7b88
MistressAlison <alisonmoons@gmail.com>
2021-07-30 15:56:48 -0300

    Added Infatuate - Applies Vuln and Weak

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Infatuate.java
@@ -0,0 +1,67 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ElementalPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.combat.EmptyStanceEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Infatuate extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Infatuate.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int EFFECT = 1;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Infatuate() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseMagicNumber = magicNumber = EFFECT;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            this.addToBot(new ApplyPowerAction(aM, p, new VulnerablePower(aM, magicNumber, false), magicNumber, true));
+            this.addToBot(new ApplyPowerAction(aM, p, new WeakPower(aM, magicNumber, false), magicNumber, true));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            initializeDescription();
+        }
+    }
+}

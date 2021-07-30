921fd80b144e1448f59adbbb4afac48c73bfba68
MistressAlison <alisonmoons@gmail.com>
2021-07-30 15:50:47 -0300

    Added Black Rose - Apply 10(15) Shadow Split to ALL enemies.

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlackRose.java
@@ -0,0 +1,67 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractShadowCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ShadowPower;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.CardPoofEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class BlackRose extends AbstractShadowCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(BlackRose.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int SPLIT = 10;
+    private static final int UPGRADE_PLUS_SPLIT = 5;
+
+    // /STAT DECLARATION/
+
+    public BlackRose() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = SPLIT;
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                this.addToBot(new VFXAction(new CardPoofEffect(aM.hb.cX, aM.hb.cY)));
+                this.addToBot(new ApplyPowerAction(aM, p, new ShadowPower(aM, magicNumber), magicNumber, true));
+            }
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_SPLIT);
+            initializeDescription();
+        }
+    }
+}

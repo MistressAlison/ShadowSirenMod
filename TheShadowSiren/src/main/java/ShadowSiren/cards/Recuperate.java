f54e14d2d1e116abe1b4ae95f6484ba250046153
MistressAlison <alisonmoons@gmail.com>
2021-07-30 18:29:37 -0300

    Added Recuperate - Gains block and cleans a debuff (or draws a card if you have none)

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Recuperate.java
@@ -0,0 +1,89 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cardModifiers.BideModifier;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.characters.Vivian;
+import basemod.helpers.CardModifierManager;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Recuperate extends AbstractDynamicCard implements ModularDescription {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Recuperate.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.SELF;
+    private static final AbstractCard.CardType TYPE = CardType.SKILL;
+    public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int CARDS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Recuperate() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        block = baseBlock = BLOCK;
+        magicNumber = baseMagicNumber = CARDS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, block));
+        int cleansed = 0;
+        for (AbstractPower pow : p.powers) {
+            if (pow.type == AbstractPower.PowerType.DEBUFF && !(pow instanceof InvisiblePower)) {
+                cleansed++;
+                this.addToBot(new RemoveSpecificPowerAction(p, p, pow));
+                if (cleansed == magicNumber) {
+                    break;
+                }
+            }
+        }
+        if (cleansed < magicNumber) {
+            this.addToBot(new DrawCardAction(magicNumber-cleansed));
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}

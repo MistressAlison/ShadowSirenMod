fa19a182d3a95d64333ec60ecc12ee41d03d7074
MistressAlison <alisonmoons@gmail.com>
2021-07-30 18:28:52 -0300

    Added Flash Freeze - Applies Freeze to ALL enemies

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/FlashFreeze.java
@@ -0,0 +1,70 @@
+package ShadowSiren.cards;
+
+import IconsAddon.actions.GainCustomBlockAction;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.FreezePower;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class FlashFreeze extends AbstractIceCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(FlashFreeze.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 3;
+    private static final int UPGRADE_COST = 2;
+    private static final int FREEZE = 1;
+
+    // /STAT DECLARATION/
+
+    public FlashFreeze() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = FREEZE;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.WHITE.cpy(), true));
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                this.addToBot(new ApplyPowerAction(aM, p, new FreezePower(aM, magicNumber), magicNumber, true));
+                aM.tint.color = Color.BLUE.cpy();
+                aM.tint.changeColor(Color.WHITE.cpy());
+            }
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+            initializeDescription();
+        }
+    }
+}

301337161a1b0a45943901da5adc4d1db3ca0c97
MistressAlison <alisonmoons@gmail.com>
2021-07-31 18:12:30 -0300

    Fire Damage no longer uses an action

--- TheShadowSiren/src/main/java/ShadowSiren/damageModifiers/FireDamage.java
+++ TheShadowSiren/src/main/java/ShadowSiren/damageModifiers/FireDamage.java
@@ -27,22 +27,15 @@ public class FireDamage extends AbstractVivianDamageModifier {
 
     @Override
     public void onDamageModifiedByBlock(DamageInfo info, int unblockedAmount, int blockedAmount, AbstractCreature targetHit) {
-        this.addToTop(new AbstractGameAction() {
-            @Override
-            public void update() {
-                targetHit.maxHealth -= unblockedAmount;
-                if (targetHit.maxHealth <= 0) {
-                    targetHit.maxHealth = 1;
-                }
-                if (targetHit.currentHealth > targetHit.maxHealth) {
-                    targetHit.currentHealth = targetHit.maxHealth;
-                }
-                targetHit.healthBarUpdatedEvent();
-                AbstractDungeon.effectList.add(new FlashAtkImgEffect(targetHit.hb.cX, targetHit.hb.cY, AbstractGameAction.AttackEffect.FIRE));
-                this.isDone = true;
-            }
-        });
-
+        targetHit.maxHealth -= unblockedAmount;
+        if (targetHit.maxHealth <= 0) {
+            targetHit.maxHealth = 1;
+        }
+        if (targetHit.currentHealth > targetHit.maxHealth) {
+            targetHit.currentHealth = targetHit.maxHealth;
+        }
+        targetHit.healthBarUpdatedEvent();
+        AbstractDungeon.effectList.add(new FlashAtkImgEffect(targetHit.hb.cX, targetHit.hb.cY, AbstractGameAction.AttackEffect.FIRE));
     }
 
     @Override

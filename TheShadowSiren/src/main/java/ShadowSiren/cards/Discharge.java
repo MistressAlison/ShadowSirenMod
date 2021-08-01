364014f383d398ca48635c7f09ba7ef9c8da61d6
MistressAlison <alisonmoons@gmail.com>
2021-08-01 13:40:00 -0300

    Reimplemented Discharge

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Discharge.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Discharge.java
@@ -1,23 +1,26 @@
-package ShadowSiren.oldStuff.cards;
+package ShadowSiren.cards;
 
+import IconsAddon.util.DamageModifierHelper;
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.oldStuff.cards.abstractCards.AbstractHyperCard;
-import ShadowSiren.oldStuff.cards.dualityCards.hyper.DischargeDual;
+import ShadowSiren.cards.abstractCards.AbstractElectricCard;
 import ShadowSiren.cards.interfaces.ChargeMultiEffect;
 import ShadowSiren.cards.interfaces.MagicAnimation;
 import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ChargePower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Discharge extends AbstractHyperCard implements MagicAnimation, ChargeMultiEffect {
+public class Discharge extends AbstractElectricCard implements ChargeMultiEffect {
 
     // TEXT DECLARATION
 
@@ -35,23 +38,46 @@ public class Discharge extends AbstractHyperCard implements MagicAnimation, Char
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int DAMAGE = 4;
+    private static final int DAMAGE = 5;
     private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int CHARGE_MULTI = 2;
+    private static final int UPGRADE_PLUS_CHARGE_MULTI = 1;
 
     // /STAT DECLARATION/
 
 
     public Discharge() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new DischargeDual());
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         damage = baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = CHARGE_MULTI;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new SFXAction("THUNDERCLAP", 0.05F));
-        this.addToBot(new VFXAction(new LightningEffect(m.drawX, m.drawY), 0.05F));
-        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.LIGHTNING));
+        if (hasCharge()) {
+            this.addToBot(new SFXAction("THUNDERCLAP", 0.05F));
+            this.addToBot(new VFXAction(new LightningEffect(m.drawX, m.drawY), 0.05F));
+        }
+        this.addToBot(new DamageAction(m,  DamageModifierHelper.makeBoundDamageInfo(this, p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+    }
+
+    private boolean hasCharge() {
+        for (AbstractPower pow : AbstractDungeon.player.powers) {
+            if (pow instanceof ChargePower) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void triggerOnGlowCheck() {
+        super.triggerOnGlowCheck();
+        if (hasCharge()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
     }
 
     //Upgraded stats.
@@ -59,14 +85,14 @@ public class Discharge extends AbstractHyperCard implements MagicAnimation, Char
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_PLUS_DMG);
+            //upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_CHARGE_MULTI);
             initializeDescription();
-            super.upgrade();
         }
     }
 
     @Override
     public float getChargeMultiplier() {
-        return 2;
+        return magicNumber;
     }
 }

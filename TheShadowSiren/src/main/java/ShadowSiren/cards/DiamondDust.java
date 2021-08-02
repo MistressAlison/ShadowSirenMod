1c396a0a70aed3f1b3c5903745519dd3f3c4f194
MistressAlison <alisonmoons@gmail.com>
2021-08-02 19:20:11 -0300

    Added Diamond Dist - Whenever you apply Chill, gain Block

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/DiamondDust.java
@@ -0,0 +1,57 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.DiamondDustPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class DiamondDust extends AbstractIceCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(DiamondDust.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderPower.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+
+    // /STAT DECLARATION/
+
+
+    public DiamondDust() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = EFFECT;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new DiamondDustPower(p, magicNumber)));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            initializeDescription();
+        }
+    }
+}

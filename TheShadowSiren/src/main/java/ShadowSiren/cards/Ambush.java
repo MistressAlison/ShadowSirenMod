9d945a980540a6dd9cf1948bb4fc417b20be717f
MistressAlison <alisonmoons@gmail.com>
2021-08-02 10:48:39 -0300

    Reimplemented Ambush

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Ambush.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Ambush.java
@@ -1,22 +1,21 @@
-package ShadowSiren.oldStuff.cards;
+package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.oldStuff.cards.abstractCards.AbstractShadowCard;
-import ShadowSiren.oldStuff.cards.dualityCards.veil.AmbushDual;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.FreezePower;
 import com.evacipated.cardcrawl.mod.stslib.powers.StunMonsterPower;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Ambush extends AbstractShadowCard {
+public class Ambush extends AbstractDynamicCard {
 
     // TEXT DECLARATION
 
@@ -32,27 +31,27 @@ public class Ambush extends AbstractShadowCard {
     private static final AbstractCard.CardType TYPE = CardType.SKILL;
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
-    private static final int COST = 1;
-    private static final int DAMAGE = 5;
-    private static final int UPGRADE_PLUS_DAMAGE = 2;
-    private static final int VULN = 1;
-    private static final int UPGRADE_PLUS_VULN = 1;
+    private static final int COST = 0;
+    private static final int VIGOR = 4;
+    private static final int UPGRADE_PLUS_VIGOR = 2;
+    private static final int ENERGY = 1;
+    private static final int UPGRADE_PLUS_ENERGY = 1;
 
     // /STAT DECLARATION/
 
 
     public Ambush() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, new AmbushDual());
-        magicNumber = baseMagicNumber = VULN;
-        secondMagicNumber = baseSecondMagicNumber = DAMAGE;
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = VIGOR;
+        secondMagicNumber = baseSecondMagicNumber = ENERGY;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false)));
         if (!m.isDeadOrEscaped() && (m.getIntentBaseDmg() > 0 || m.hasPower(StunMonsterPower.POWER_ID) || m.hasPower(FreezePower.POWER_ID))) {
-            this.addToBot(new LoseHPAction(m, p, secondMagicNumber, AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+            this.addToBot(new GainEnergyAction(secondMagicNumber));
+            this.addToBot(new ApplyPowerAction(p, p, new VigorPower(m, magicNumber)));
         }
     }
 
@@ -61,10 +60,10 @@ public class Ambush extends AbstractShadowCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeSecondMagicNumber(UPGRADE_PLUS_DAMAGE);
-            upgradeMagicNumber(UPGRADE_PLUS_VULN);
+            upgradeSecondMagicNumber(UPGRADE_PLUS_ENERGY);
+            upgradeMagicNumber(UPGRADE_PLUS_VIGOR);
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
-            super.upgrade();
         }
     }
 }

9c84bd4f9e735d8c79f3899111f90188f4e4cd8b
MistressAlison <alisonmoons@gmail.com>
2021-08-02 23:16:42 -0300

    Added Plasma Beam - Deals Fire Electric damage to ALL enemies

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/PlasmaBeam.java
@@ -0,0 +1,83 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.DamageModifierHelper;
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractMultiElementCard;
+import ShadowSiren.cards.interfaces.MagicAnimation;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.ElectricDamage;
+import ShadowSiren.damageModifiers.FireDamage;
+import ShadowSiren.vfx.ElementParticleEffect;
+import basemod.helpers.VfxBuilder;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class PlasmaBeam extends AbstractMultiElementCard implements MagicAnimation {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(PlasmaBeam.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_PLUS_DMG = 3;
+
+    // /STAT DECLARATION/
+
+    public PlasmaBeam() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        isMultiDamage = true;
+        exhaust = true;
+        DamageModifierManager.addModifier(this, new FireDamage());
+        DamageModifierManager.addModifier(this, new ElectricDamage());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new SFXAction("ORB_PLASMA_CHANNEL", 0.2f));
+        this.addToBot(new VFXAction(p, new VfxBuilder(ImageMaster.GLOW_SPARK_2, p.hb.cX+p.hb.width/2, p.hb.cY, 0.3f)
+                .scale(1.0f, 3.0f, VfxBuilder.Interpolations.FADE)
+                .setColor(Color.GOLD)
+                .moveX(p.hb.cX+p.hb.width/2, Settings.WIDTH, VfxBuilder.Interpolations.EXP5IN)
+                .emitEvery((x,y) -> new ElementParticleEffect(new FireDamage(), x+(MathUtils.random(10,30)*Settings.scale*MathUtils.randomSign()), y+(MathUtils.random(10,30)*Settings.scale*MathUtils.randomSign()), 0, 0, 1.5f, 0), 0.01f)
+                .emitEvery((x,y) -> new ElementParticleEffect(new ElectricDamage(), x, y, 0, 0, 1.5f, 0), 0.01f)
+                .rotate(-400f)
+                .build(), 0.3f, true));
+        this.addToBot(DamageModifierHelper.makeModifiedDamageAllEnemiesAction(this, p, damage, damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}

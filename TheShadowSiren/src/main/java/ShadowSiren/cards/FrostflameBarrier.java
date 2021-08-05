0cad698803303850a00306ddbe524fe2d9e98996
Alison Turner <alisonmoons@gmail.com>
2021-08-05 19:08:00 -0300

    Added Frostflame Barrier - Gain Ice Fire Block. For making sure compound block actually works

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/FrostflameBarrier.java
@@ -0,0 +1,69 @@
+package ShadowSiren.cards;
+
+import IconsAddon.actions.GainCustomBlockAction;
+import IconsAddon.util.BlockModifierManager;
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.FireBlock;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.cards.abstractCards.AbstractMultiElementCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import ShadowSiren.damageModifiers.FireDamage;
+import ShadowSiren.damageModifiers.IceDamage;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class FrostflameBarrier extends AbstractMultiElementCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(FrostflameBarrier.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 6;
+    private static final int UPGRADE_PLUS_BLOCK = 3;
+
+    // /STAT DECLARATION/
+
+    public FrostflameBarrier() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+        DamageModifierManager.addModifier(this, new IceDamage(AbstractVivianDamageModifier.TipType.BLOCK));
+        DamageModifierManager.addModifier(this, new FireDamage(AbstractVivianDamageModifier.TipType.BLOCK));
+        BlockModifierManager.addModifier(this, new IceBlock());
+        BlockModifierManager.addModifier(this, new FireBlock());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainCustomBlockAction(this, p, block));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+}

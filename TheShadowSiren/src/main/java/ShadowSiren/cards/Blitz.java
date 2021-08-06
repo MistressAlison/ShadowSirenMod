6d9bf37792c9e1037ba180debb41c5a4f67ecd10
Alison Turner <alisonmoons@gmail.com>
2021-08-05 23:09:43 -0300

    Added Blitz - Deals damage then increases hits and cost. Reshuffles, but resets when discarded.

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Blitz.java
@@ -0,0 +1,95 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.powers.ElementalPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Blitz extends AbstractDynamicCard implements ModularDescription {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Blitz.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int HITS = 1;
+
+    // /STAT DECLARATION/
+
+    public Blitz() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = HITS;
+        shuffleBackIntoDrawPile = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
+        }
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                Blitz.this.modifyCostForCombat(1);
+                Blitz.this.magicNumber += 1;
+                Blitz.this.baseMagicNumber += 1;
+                this.isDone = true;
+            }
+        });
+    }
+
+    @Override
+    public void onMoveToDiscard() {
+        this.modifyCostForCombat(1-magicNumber);
+        this.isCostModified = cost != COST;
+        this.isCostModifiedForTurn = costForTurn != COST;
+        this.magicNumber = this.baseMagicNumber = HITS;
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}

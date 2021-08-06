e8040d5c49c7055410691d759e887db83ea55d5d
Alison Turner <alisonmoons@gmail.com>
2021-08-05 23:10:18 -0300

    Added Brick Breaker - Applies 1 Block, then adds Hammers to your hand.

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BrickBreaker.java
@@ -0,0 +1,68 @@
+package ShadowSiren.cards;
+
+import IconsAddon.actions.GainCustomBlockAction;
+import IconsAddon.util.BlockModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.cards.tempCards.Hammer;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class BrickBreaker extends AbstractIceCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(BrickBreaker.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int BLOCK = 1;
+    private static final int CARDS = 2;
+    private static final int UPGRADE_PLUS_CARDS = 1;
+
+    // /STAT DECLARATION/
+
+    public BrickBreaker() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
+        baseBlock = block = BLOCK;
+        magicNumber = baseMagicNumber = CARDS;
+        this.cardsToPreview = new Hammer();
+        grabPreviewKeywords();
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(m, p, block));
+        this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy(), magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_CARDS);
+            initializeDescription();
+        }
+    }
+}

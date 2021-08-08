91032845c9bdb8d56a818fbb7e6fb62d0b369479
Alison Turner <alisonmoons@gmail.com>
2021-08-08 20:04:34 -0300

    Added Trick - Deals Shadow Damage and clears a debuff

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Trick.java
@@ -0,0 +1,108 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractShadowCard;
+import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.characters.Vivian;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Trick extends AbstractShadowCard implements ModularDescription {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Trick.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+
+    // /TEXT DECLARATION/
+
+    // STAT DECLARATION
+
+    private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.ENEMY;
+    private static final AbstractCard.CardType TYPE = CardType.SKILL;
+    public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 6;
+    private static final int UPGRADE_PLUS_DAMAGE = 3;
+    private static final int CARDS = 1;
+
+    // /STAT DECLARATION/
+
+
+    public Trick() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        damage = baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = CARDS;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        int cleansed = 0;
+        for (AbstractPower pow : p.powers) {
+            if (pow.type == AbstractPower.PowerType.DEBUFF && !(pow instanceof InvisiblePower)) {
+                cleansed++;
+                this.addToBot(new RemoveSpecificPowerAction(p, p, pow));
+                if (cleansed == magicNumber) {
+                    break;
+                }
+            }
+        }
+        if (cleansed < magicNumber) {
+            this.addToBot(new DrawCardAction(magicNumber - cleansed));
+        }
+    }
+
+    private boolean hasDebuff() {
+        for (AbstractPower pow : AbstractDungeon.player.powers) {
+            if (pow.type == AbstractPower.PowerType.DEBUFF) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void triggerOnGlowCheck() {
+        super.triggerOnGlowCheck();
+        if (hasDebuff()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}

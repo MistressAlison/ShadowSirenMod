cd1c6730d37ebc131d35c5f079675e47fd0b5aa2
Alison Turner <alisonmoons@gmail.com>
2021-08-08 20:03:49 -0300

    Added Blast Burn - Deals Fire damage and sets target max HP 1 to (they get Temp HP equal to HP lost)

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlastBurn.java
@@ -0,0 +1,72 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.actions.BurnToAshEffect;
+import ShadowSiren.cards.abstractCards.AbstractFireCard;
+import ShadowSiren.characters.Vivian;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class BlastBurn extends AbstractFireCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(BlastBurn.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int DAMAGE = 15;
+    private static final int UPGRADE_PLUS_DMG = 5;
+
+    // /STAT DECLARATION/
+
+    public BlastBurn() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new VFXAction(new BurnToAshEffect(m.hb.cX, m.hb.cY)));
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                int loss = m.currentHealth-1;
+                m.decreaseMaxHealth(m.maxHealth);
+                this.addToTop(new AddTemporaryHPAction(m, p, loss));
+                this.isDone = true;
+            }
+        });
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}

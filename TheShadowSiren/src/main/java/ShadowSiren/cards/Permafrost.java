da388d01650bd6d252c1cbb2bc1cd818483e8284
Alison Turner <alisonmoons@gmail.com>
2021-08-13 13:56:12 -0300

    Added Permafrost, applies Weak to ALL then gains Ice Block equal to Weak applied

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Permafrost.java
@@ -0,0 +1,80 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.BlockModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import ShadowSiren.powers.FreezePower;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.*;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Permafrost extends AbstractIceCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Permafrost.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int WEAK = 2;
+    private static final int UPGRADE_PLUS_WEAK = 1;
+
+    // /STAT DECLARATION/
+
+    public Permafrost() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.DAMAGE_AND_BLOCK);
+        magicNumber = baseMagicNumber = WEAK;
+        BlockModifierManager.addModifier(this, new IceBlock());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.WHITE.cpy(), true));
+        int sum = 0;
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                this.addToBot(new ApplyPowerAction(aM, p, new WeakPower(aM, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+                if (!aM.hasPower(ArtifactPower.POWER_ID)) {
+                    sum += magicNumber;
+                }
+            }
+        }
+        if (sum > 0) {
+            this.addToBot(new GainBlockAction(p, sum));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_WEAK);
+            initializeDescription();
+        }
+    }
+}

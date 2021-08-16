abe6f7e6a6c59eaf51b91a80389fb0228c48d708
Alison Turner <alisonmoons@gmail.com>
2021-08-16 12:41:39 -0300

    Black Roses now causes targets to lose HP equal to their SS

--- TheShadowSiren/src/main/java/ShadowSiren/cards/BlackRose.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlackRose.java
@@ -7,6 +7,7 @@ import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
 import ShadowSiren.powers.ShadowPower;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -34,14 +35,12 @@ public class BlackRose extends AbstractShadowCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int SPLIT = 10;
-    private static final int UPGRADE_PLUS_SPLIT = 5;
 
     // /STAT DECLARATION/
 
+    //TODO rework
     public BlackRose() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.DAMAGE_AND_BLOCK);
-        magicNumber = baseMagicNumber = SPLIT;
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         exhaust = true;
     }
 
@@ -50,8 +49,10 @@ public class BlackRose extends AbstractShadowCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
             if (!aM.isDeadOrEscaped()) {
-                this.addToBot(new VFXAction(new CardPoofEffect(aM.hb.cX, aM.hb.cY)));
-                this.addToBot(new ApplyPowerAction(aM, p, new ShadowPower(aM, magicNumber), magicNumber, true));
+                if (aM.hasPower(ShadowPower.POWER_ID)) {
+                    this.addToBot(new VFXAction(new CardPoofEffect(aM.hb.cX, aM.hb.cY)));
+                    this.addToBot(new LoseHPAction(aM, p, aM.getPower(ShadowPower.POWER_ID).amount));
+                }
             }
         }
     }
@@ -61,7 +62,8 @@ public class BlackRose extends AbstractShadowCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_SPLIT);
+            this.exhaust = false;
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
     }

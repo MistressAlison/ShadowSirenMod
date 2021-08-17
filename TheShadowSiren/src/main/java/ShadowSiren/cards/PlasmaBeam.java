c43b50f09bd9cc96f3664129761fed8dbbda5d92
Alison Turner <alisonmoons@gmail.com>
2021-08-16 22:44:05 -0300

    Plasma Beam buffed to Uncommon

--- TheShadowSiren/src/main/java/ShadowSiren/cards/PlasmaBeam.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/PlasmaBeam.java
@@ -36,7 +36,7 @@ public class PlasmaBeam extends AbstractMultiElementCard implements MagicAnimati
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;

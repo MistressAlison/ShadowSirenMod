53f1e485599d7e714313680ed6f17ec16ca26286
Alison Turner <alisonmoons@gmail.com>
2021-08-16 22:15:41 -0300

    Added Glaciate - Deals Ice damage, if the target has weak, increases the amount of hits it deals

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Glaciate.java
@@ -0,0 +1,107 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.characters.Vivian;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Glaciate extends AbstractIceCard implements ModularDescription {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Glaciate.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 2;
+    private static final int HITS = 1;
+    private static final int INCREASE = 1;
+
+    // /STAT DECLARATION/
+
+    public Glaciate() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = HITS;
+        isMultiDamage = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
+        }
+        if (m.hasPower(WeakPower.POWER_ID)) {
+            this.addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    upgradeMagicNumber(1);
+                    this.isDone = true;
+                }
+            });
+        }
+    }
+
+    private boolean enemyHasWeak() {
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped() && aM.hasPower(WeakPower.POWER_ID)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void triggerOnGlowCheck() {
+        super.triggerOnGlowCheck();
+        if (enemyHasWeak()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        } else {
+            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}

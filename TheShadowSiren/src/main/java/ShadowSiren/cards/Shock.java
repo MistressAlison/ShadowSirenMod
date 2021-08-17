86fcf00b0ba19822755d0e5a9b09f7beb0424e9a
Alison Turner <alisonmoons@gmail.com>
2021-08-16 22:14:10 -0300

    Added Shock - Cantrip that gives very little Electric Block

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Shock.java
@@ -0,0 +1,79 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.BlockModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.ElectricBlock;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractElectricCard;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Shock extends AbstractElectricCard implements ModularDescription {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Shock.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int BLOCK = 2;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int DRAW = 1;
+
+    // /STAT DECLARATION/
+
+    public Shock() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
+        baseBlock = block = BLOCK;
+        magicNumber = baseMagicNumber = DRAW;
+        BlockModifierManager.addModifier(this, new ElectricBlock());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new DrawCardAction(magicNumber));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public void changeDescription() {
+        if (DESCRIPTION != null) {
+            if (magicNumber > 1) {
+                rawDescription = UPGRADE_DESCRIPTION;
+            } else {
+                rawDescription = DESCRIPTION;
+            }
+        }
+    }
+}

1c027b706e101e419cf56a4c1b17861f2e3e16d0
Alison Turner <alisonmoons@gmail.com>
2021-08-16 22:06:07 -0300

    Added art to Black Rose

--- TheShadowSiren/src/main/java/ShadowSiren/cards/BlackRose.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlackRose.java
@@ -21,7 +21,7 @@ public class BlackRose extends AbstractShadowCard {
     // TEXT DECLARATION
 
     public static final String ID = ShadowSirenMod.makeID(BlackRose.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    public static final String IMG = makeCardPath("BlackRose.png");
     // Setting the image as as easy as can possibly be now. You just need to provide the image name
 
     // /TEXT DECLARATION/

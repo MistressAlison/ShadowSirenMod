76de0f6afcceb9517791f4acc34b11fba13501c1
Alison Turner <alisonmoons@gmail.com>
2021-08-16 22:13:30 -0300

    Added art to Shadow Sneak

--- TheShadowSiren/src/main/java/ShadowSiren/cards/ShadowSneak.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ShadowSneak.java
@@ -20,7 +20,7 @@ public class ShadowSneak extends AbstractShadowCard {
     // TEXT DECLARATION
 
     public static final String ID = ShadowSirenMod.makeID(ShadowSneak.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    public static final String IMG = makeCardPath("DarkEmbrace.png");
     // Setting the image as as easy as can possibly be now. You just need to provide the image name
 
     // /TEXT DECLARATION/

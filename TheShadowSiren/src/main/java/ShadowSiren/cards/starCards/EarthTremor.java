49b37dd0376b05559da1ad6d02ac9a062979b4cd
Alison Turner <alisonmoons@gmail.com>
2021-08-21 14:39:00 -0300

    Added 2 star cards for testing

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/starCards/EarthTremor.java
@@ -0,0 +1,68 @@
+package ShadowSiren.cards.starCards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractStarCard;
+import ShadowSiren.characters.Vivian;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class EarthTremor extends AbstractStarCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(EarthTremor.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 10;
+    private static final int UPGRADE_PLUS_DAMAGE = 5;
+    // /STAT DECLARATION/
+
+
+    public EarthTremor() {
+        super(ID, IMG, TYPE, COLOR, TARGET);
+        damage = baseDamage = DAMAGE;
+        isMultiDamage = true;
+        purgeOnUse = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.HIGH, ScreenShake.ShakeDur.MED, false);
+        this.addToBot(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public int getSpawnCost() {
+        return 1;
+    }
+}
---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/starCards/SweetTreat.java
@@ -0,0 +1,66 @@
+package ShadowSiren.cards.starCards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractStarCard;
+import ShadowSiren.characters.Vivian;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class SweetTreat extends AbstractStarCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(SweetTreat.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int HEAL = 5;
+    private static final int UPGRADE_PLUS_HEAL = 3;
+    private static final int ENERGY = 1;
+    private static final int UPGRADE_PLUS_ENERGY = 1;
+    // /STAT DECLARATION/
+
+
+    public SweetTreat() {
+        super(ID, IMG, TYPE, COLOR, TARGET);
+        baseMagicNumber = magicNumber = HEAL;
+        baseSecondMagicNumber = secondMagicNumber = ENERGY;
+        purgeOnUse = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new HealAction(p, p, magicNumber));
+        this.addToBot(new GainEnergyAction(secondMagicNumber));
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_HEAL);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public int getSpawnCost() {
+        return 1;
+    }
+}

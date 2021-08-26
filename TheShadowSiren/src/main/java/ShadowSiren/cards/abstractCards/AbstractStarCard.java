1b778075517fd9f2403a4392989208e3a6bf89fc
Alison Turner <alisonmoons@gmail.com>
2021-08-26 13:00:23 -0300

    Star cards now have tooltips

--- TheShadowSiren/src/main/java/ShadowSiren/cards/abstractCards/AbstractStarCard.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/abstractCards/AbstractStarCard.java
@@ -1,10 +1,18 @@
 package ShadowSiren.cards.abstractCards;
 
 import ShadowSiren.ShadowSirenMod;
+import basemod.BaseMod;
+import basemod.helpers.TooltipInfo;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class AbstractStarCard extends AbstractDynamicCard {
 
-    public AbstractStarCard(String id, String img, CardType type, CardColor color, CardTarget target) {
+    private static ArrayList<TooltipInfo> starTooltip;
+
+    public AbstractStarCard(String id, String img, CardType type, CardColor color, CardTarget target, boolean showStarCost) {
         super(id, img, 0, type, color, CardRarity.SPECIAL, target);
         switch (type) {
             case ATTACK:
@@ -17,7 +25,25 @@ public abstract class AbstractStarCard extends AbstractDynamicCard {
                 this.setBackgroundTexture(ShadowSirenMod.SKILL_STAR, ShadowSirenMod.SKILL_STAR_PORTRAIT);
                 break;
         }
+        if (showStarCost) {
+            this.rawDescription = EXTENDED_DESCRIPTION[0];
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public List<TooltipInfo> getCustomTooltipsTop() {
+        if (starTooltip == null)
+        {
+            starTooltip = new ArrayList<>();
+            starTooltip.add(new TooltipInfo(BaseMod.getKeywordTitle("shadowsiren:star"), BaseMod.getKeywordDescription("shadowsiren:star")));
+            starTooltip.add(new TooltipInfo(BaseMod.getKeywordTitle("shadowsiren:star_power"), BaseMod.getKeywordDescription("shadowsiren:star_power")));
+        }
+        List<TooltipInfo> compoundList = new ArrayList<>(starTooltip);
+        if (super.getCustomTooltipsTop() != null) compoundList.addAll(super.getCustomTooltipsTop());
+        return compoundList;
     }
 
     public abstract int getSpawnCost();
+
 }

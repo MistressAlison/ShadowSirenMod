3ef010f4ce5734e59badcb5a21f3eaec0c92c6ec
Alison Turner <alisonmoons@gmail.com>
2021-08-26 13:01:07 -0300

    Added Clock Out - Applies negative Str to ALL enemies

--- TheShadowSiren/src/main/java/ShadowSiren/cards/starCards/ClockOut.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/starCards/ClockOut.java
@@ -1,4 +1,85 @@
 package ShadowSiren.cards.starCards;
 
-public class ClockOut {
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractStarCard;
+import ShadowSiren.characters.Vivian;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import com.megacrit.cardcrawl.vfx.combat.TimeWarpTurnEndEffect;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class ClockOut extends AbstractStarCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(ClockOut.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int EFFECT = 3;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
+    // /STAT DECLARATION/
+
+
+    public ClockOut() {
+        this(false);
+    }
+
+    public ClockOut(boolean showStarCost) {
+        super(ID, IMG, TYPE, COLOR, TARGET, showStarCost);
+        baseMagicNumber = magicNumber = EFFECT;
+        purgeOnUse = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                CardCrawlGame.sound.play("POWER_TIME_WARP", 0.05F);
+                AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.GOLD, true));
+                AbstractDungeon.topLevelEffectsQueue.add(new TimeWarpTurnEndEffect());
+                this.isDone = true;
+            }
+        });
+        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
+            if (!aM.isDeadOrEscaped()) {
+                this.addToBot(new ApplyPowerAction(aM, p, new StrengthPower(aM, -magicNumber), -magicNumber, true));
+            }
+        }
+    }
+
+    //Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public int getSpawnCost() {
+        return COST;
+    }
 }

4c6274e258ba2e2266f4328c9ebf1265a07a1f44
Alison Turner <alisonmoons@gmail.com>
2021-08-28 18:06:55 -0300

    Dire Strike now Special

--- TheShadowSiren/src/main/java/ShadowSiren/cards/tempCards/DireStrike.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/tempCards/DireStrike.java
@@ -28,7 +28,7 @@ public class DireStrike extends AbstractDynamicCard implements TempCard {
 
     // STAT DECLARATION
 
-    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardRarity RARITY = CardRarity.SPECIAL;
     private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;

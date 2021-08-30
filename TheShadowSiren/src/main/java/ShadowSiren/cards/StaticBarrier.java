5116d28b04767c0d7daa118864542c639e2025f6
Alison Turner <alisonmoons@gmail.com>
2021-08-29 23:08:04 -0300

    Static Barrier now gives 5(7) Block twice and retains

--- TheShadowSiren/src/main/java/ShadowSiren/cards/StaticBarrier.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/StaticBarrier.java
@@ -4,10 +4,12 @@ import IconsAddon.util.BlockModifierManager;
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.blockTypes.ElectricBlock;
 import ShadowSiren.blockTypes.FireBlock;
+import ShadowSiren.cardModifiers.ChargeModifier;
 import ShadowSiren.cards.abstractCards.AbstractElectricCard;
 import ShadowSiren.cards.abstractCards.AbstractFireCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -34,21 +36,27 @@ public class StaticBarrier extends AbstractElectricCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 2;
-    private static final int BLOCK = 10;
-    private static final int UPGRADE_PLUS_BLOCK = 4;
+    private static final int BLOCK = 5;
+    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int TIMES = 2;
 
     // /STAT DECLARATION/
 
     public StaticBarrier() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
         baseBlock = block = BLOCK;
+        magicNumber = baseMagicNumber = TIMES;
         BlockModifierManager.addModifier(this, new ElectricBlock());
+        CardModifierManager.addModifier(this, new ChargeModifier(1, true));
+        selfRetain = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new GainBlockAction(p, block));
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new GainBlockAction(p, block));
+        }
     }
 
     // Upgraded stats.

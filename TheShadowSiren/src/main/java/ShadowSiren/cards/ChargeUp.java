3fd6967ef59dc93db3098529214ec860703823c1
Alison Turner <alisonmoons@gmail.com>
2021-08-29 23:26:42 -0300

    Charge Up now selects a card and applies Charge to it. Slightly more reasonable

--- TheShadowSiren/src/main/java/ShadowSiren/cards/ChargeUp.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ChargeUp.java
@@ -1,21 +1,20 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
+import ShadowSiren.cardModifiers.ChargeModifier;
 import ShadowSiren.cards.abstractCards.AbstractElectricCard;
-import ShadowSiren.cards.interfaces.ModularDescription;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.powers.ChargePower;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import basemod.helpers.CardModifierManager;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class ChargeUp extends AbstractElectricCard implements ModularDescription {
+public class ChargeUp extends AbstractElectricCard {
 
     // TEXT DECLARATION
 
@@ -33,7 +32,7 @@ public class ChargeUp extends AbstractElectricCard implements ModularDescription
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int EFFECT = 1;
+    private static final int EFFECT = 2;
     private static final int UPGRADE_PLUS_EFFECT = 1;
 
     // /STAT DECLARATION/
@@ -47,7 +46,18 @@ public class ChargeUp extends AbstractElectricCard implements ModularDescription
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(p, p, new ChargePower(p, magicNumber)));
+        this.addToBot(new SelectCardsInHandAction(1, CardCrawlGame.languagePack.getCardStrings(ShadowSirenMod.makeID("ChargeModifier")).NAME, true, true, c -> c.baseDamage >= 0 || c.baseBlock >= 0, l -> l.forEach(this::onEquip)));
+    }
+
+    public void onEquip(AbstractCard c) {
+        this.addToTop(new AbstractGameAction() {
+            @Override
+            public void update() {
+                CardModifierManager.addModifier(c, new ChargeModifier(magicNumber));
+                //AbstractDungeon.player.hand.moveToDeck(c, true);
+                this.isDone = true;
+            }
+        });
     }
 
     //Upgraded stats.
@@ -57,17 +67,6 @@ public class ChargeUp extends AbstractElectricCard implements ModularDescription
             upgradeName();
             upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
-        }
-    }
-
-    @Override
-    public void changeDescription() {
-        if (DESCRIPTION != null) {
-            if (magicNumber > 1) {
-                rawDescription = UPGRADE_DESCRIPTION;
-            } else {
-                rawDescription = DESCRIPTION;
-            }
         }
     }
 }

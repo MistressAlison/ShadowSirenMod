a75db5bf34a08ca9f196250286117a3d4cdbe37e
Alison Turner <alisonmoons@gmail.com>
2021-09-03 19:20:47 -0300

    Singe now scales as damage

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Singe.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Singe.java
@@ -2,7 +2,6 @@ package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractFireCard;
-import ShadowSiren.cards.interfaces.VigorMagicBuff;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
@@ -12,7 +11,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Singe extends AbstractFireCard implements VigorMagicBuff {
+public class Singe extends AbstractFireCard {
 
     // TEXT DECLARATION
 
@@ -29,25 +28,28 @@ public class Singe extends AbstractFireCard implements VigorMagicBuff {
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int HP = 5;
-    private static final int UPGRADE_PLUS_HP = 2;
+    private static final int HP_LOSS = 5;
+    private static final int UPGRADE_PLUS_HP_LOSS = 2;
+    private static final int MULTIPLIER = 2;
+    private static final int UPGRADE_PLUS_MULTIPLIER = 1;
 
     // /STAT DECLARATION/
 
 
     public Singe() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = HP;
+        damage = baseDamage = HP_LOSS;
+        magicNumber = baseMagicNumber = MULTIPLIER;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new LoseHPAction(m, p, magicNumber, AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new LoseHPAction(m, p, damage, AbstractGameAction.AttackEffect.FIRE));
         this.addToBot(new AbstractGameAction() {
             @Override
             public void update() {
-                m.decreaseMaxHealth(magicNumber*2);
+                m.decreaseMaxHealth(damage*magicNumber);
                 this.isDone = true;
             }
         });
@@ -58,7 +60,8 @@ public class Singe extends AbstractFireCard implements VigorMagicBuff {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_HP);
+            upgradeDamage(UPGRADE_PLUS_HP_LOSS);
+            //upgradeMagicNumber(UPGRADE_PLUS_MULTIPLIER);
         }
     }
 }

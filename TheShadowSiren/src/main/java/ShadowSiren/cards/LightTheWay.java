b33ded6c0bdb2dd6db2c8538b672f52269b76cdb
Alison Turner <alisonmoons@gmail.com>
2021-09-03 19:22:21 -0300

    Added Light The Way, draws cards and adds ashes

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/LightTheWay.java
@@ -0,0 +1,62 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractFireCard;
+import ShadowSiren.cards.tempCards.Ashes;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class LightTheWay extends AbstractFireCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(LightTheWay.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 0;
+    private static final int DRAW = 2;
+    private static final int UPGRADE_PLUS_DRAW = 1;
+
+    // /STAT DECLARATION/
+
+    public LightTheWay() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.DAMAGE_AND_BLOCK);
+        magicNumber = baseMagicNumber = DRAW;
+        cardsToPreview = new Ashes();
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DrawCardAction(magicNumber));
+        this.addToBot(new MakeTempCardInDrawPileAction(cardsToPreview, 1, true, true));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_DRAW);
+            initializeDescription();
+        }
+    }
+}

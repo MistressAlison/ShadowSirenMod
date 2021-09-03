f12a5be6c1065b6442e296644cca27bd43a8bd40
Alison Turner <alisonmoons@gmail.com>
2021-09-03 19:23:37 -0300

    Chill now rolls over at 10 stacks, applying Freeze and Frosty

--- TheShadowSiren/src/main/java/ShadowSiren/powers/ChillPower.java
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/ChillPower.java
@@ -1,13 +1,12 @@
 package ShadowSiren.powers;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.relics.DataCollector;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -26,7 +25,7 @@ public class ChillPower extends AbstractPower implements CloneablePowerInterface
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     public static final int REDUCTION_PERCENT = 5;
-    public static final int MAX_REDUCTION = 50;
+    public static final int MAX_STACKS = 10;
     private static final float DECAY_RATE = 1/2f;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
@@ -64,45 +63,28 @@ public class ChillPower extends AbstractPower implements CloneablePowerInterface
     @Override
     public void onInitialApplication() {
         super.onInitialApplication();
-        if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
-            DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-            dataCollector.onApplyChill(amount);
-        }
-        //freezeCheck();
+        freezeCheck();
     }
 
     @Override
     public void stackPower(int stackAmount) {
         super.stackPower(stackAmount);
-        if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
-            DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-            dataCollector.onApplyChill(amount);
-        }
-        //freezeCheck();
+        freezeCheck();
     }
 
     public void freezeCheck() {
-        this.addToTop(new AbstractGameAction() {
-            @Override
-            public void update() {
-                if (owner.getPower(ChillPower.POWER_ID).amount >= MAX_REDUCTION) {
-                    int effect = amount / MAX_REDUCTION;
-                    this.addToTop(new ReducePowerAction(owner, owner, ChillPower.this, effect* MAX_REDUCTION));
-                    this.addToTop(new ApplyPowerAction(owner, owner, new FreezePower((AbstractMonster) owner, effect)));
-                    if (AbstractDungeon.player.hasRelic(DataCollector.ID)) {
-                        DataCollector dataCollector = (DataCollector) AbstractDungeon.player.getRelic(DataCollector.ID);
-                        dataCollector.onChillRollover(effect);
-                    }
-                }
-                this.isDone = true;
-            }
-        });
+        if (amount >= MAX_STACKS) {
+            int effect = amount / MAX_STACKS;
+            this.addToTop(new ReducePowerAction(owner, owner, this, effect * MAX_STACKS));
+            this.addToTop(new ApplyPowerAction(owner, owner, new FrostyPower(owner, effect)));
+            this.addToTop(new ApplyPowerAction(owner, owner, new FreezePower(owner, effect)));
+        }
     }
 
     @Override
     public float atDamageGive(float damage, DamageInfo.DamageType type) {
         if (type == DamageInfo.DamageType.NORMAL) {
-            return damage * (1 - Math.min(REDUCTION_PERCENT*amount, MAX_REDUCTION)/100f);
+            return Math.max(0, damage * (1 - (REDUCTION_PERCENT*amount)/100f));
         } else {
             return damage;
         }
@@ -111,18 +93,19 @@ public class ChillPower extends AbstractPower implements CloneablePowerInterface
     @Override
     public void atEndOfRound() {
         this.flashWithoutSound();
-        if (this.amount <= 1) {
-            this.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
-        } else {
-            int reduce = Math.max(1, (int)(this.amount * DECAY_RATE));
-            this.addToTop(new ReducePowerAction(owner, owner, this, reduce));
-            updateDescription();
+        this.addToTop(new ReducePowerAction(owner, owner, this, 1));
+    }
+
+    public void onIceBlockTrigger(AbstractCreature source) {
+        if (amount > 0) {
+            this.flash();
+            this.addToTop(new DamageAction(owner, new DamageInfo(source, amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.NONE, true));
         }
     }
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + Math.min(MAX_REDUCTION, (REDUCTION_PERCENT*amount)) + DESCRIPTIONS[1] + MAX_REDUCTION + DESCRIPTIONS[2];
+        description = DESCRIPTIONS[0] + REDUCTION_PERCENT*amount + DESCRIPTIONS[1] + MAX_STACKS + DESCRIPTIONS[2];
     }
 
     @Override

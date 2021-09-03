ee5ffa7c62f115c25c051d6924ccba691a022a00
Alison Turner <alisonmoons@gmail.com>
2021-09-03 19:15:55 -0300

    Added interface for Vigor buffing Block

--- TheShadowSiren/src/main/java/ShadowSiren/patches/ValueBuffPatches.java
+++ TheShadowSiren/src/main/java/ShadowSiren/patches/ValueBuffPatches.java
@@ -1,6 +1,7 @@
 package ShadowSiren.patches;
 
 import ShadowSiren.cards.interfaces.ChargeMagicBuff;
+import ShadowSiren.cards.interfaces.VigorBlockBuff;
 import ShadowSiren.cards.interfaces.VigorMagicBuff;
 import ShadowSiren.oldStuff.powers.ChargePower;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
@@ -10,25 +11,35 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
-public class MagicBuffPatches {
+public class ValueBuffPatches {
 
     @SpirePatch(clz = AbstractCard.class, method = "applyPowers")
     public static class ApplyPowersPatch {
         public static void Postfix(AbstractCard __instance) {
             //There are 2 different interfaces we can implement to get a bonus to our magic number
-            int bonus = 0;
+            int magicBonus = 0;
+            int blockBonus = 0;
             //Vigor check
             if (__instance instanceof VigorMagicBuff && AbstractDungeon.player.hasPower(VigorPower.POWER_ID)) {
-                bonus += AbstractDungeon.player.getPower(VigorPower.POWER_ID).amount;;
+                magicBonus += AbstractDungeon.player.getPower(VigorPower.POWER_ID).amount;
+            }
+            if (__instance instanceof VigorBlockBuff && AbstractDungeon.player.hasPower(VigorPower.POWER_ID)) {
+                blockBonus += AbstractDungeon.player.getPower(VigorPower.POWER_ID).amount;
             }
             //Charge check
             if (__instance instanceof ChargeMagicBuff && AbstractDungeon.player.hasPower(ChargePower.POWER_ID)) {
-                bonus += AbstractDungeon.player.getPower(ChargePower.POWER_ID).amount;;
+                magicBonus += AbstractDungeon.player.getPower(ChargePower.POWER_ID).amount;
             }
             //If our card implements either of these, we add any bonus and update the magic number accordingly
             if (__instance instanceof VigorMagicBuff || __instance instanceof ChargeMagicBuff) {
-                __instance.magicNumber = __instance.baseMagicNumber + bonus;
+                __instance.magicNumber = __instance.baseMagicNumber + magicBonus;
                 __instance.isMagicNumberModified = __instance.magicNumber != __instance.baseMagicNumber;
+                __instance.initializeDescription();
+            }
+            //TODO check if Fire Block?
+            if (__instance instanceof VigorBlockBuff) {
+                __instance.block += blockBonus;
+                __instance.isBlockModified = __instance.block != __instance.baseBlock;
                 __instance.initializeDescription();
             }
         }
@@ -38,7 +49,7 @@ public class MagicBuffPatches {
     public static class OnUseCardPatch {
         public static void Postfix(VigorPower __instance, AbstractCard card, UseCardAction action) {
             //Attacks already cause us to lose Vigor, so we need to remove it only if our card implements the Vigor buff and is not an attack
-            if (card.type != AbstractCard.CardType.ATTACK && card instanceof VigorMagicBuff) {
+            if (card.type != AbstractCard.CardType.ATTACK && (card instanceof VigorMagicBuff || card instanceof VigorBlockBuff)) {
                 __instance.flash();
                 AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(__instance.owner, __instance.owner, VigorPower.POWER_ID));
             }

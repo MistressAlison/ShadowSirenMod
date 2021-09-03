da4cc0f3f6aa64d03a57964a63f9fe4bb21084a7
Alison Turner <alisonmoons@gmail.com>
2021-09-03 19:22:54 -0300

    Added Frosty, makes targets take more damage and doesnt decay

---
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/FrostyPower.java
@@ -0,0 +1,59 @@
+package ShadowSiren.powers;
+
+import ShadowSiren.ShadowSirenMod;
+import basemod.interfaces.CloneablePowerInterface;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class FrostyPower extends AbstractPower implements CloneablePowerInterface {
+
+    public static final String POWER_ID = ShadowSirenMod.makeID("FrostyPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public static final int PERCENT_PER = 10;
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public FrostyPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.DEBUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        this.loadRegion("blur");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public float atDamageReceive(float damage, DamageInfo.DamageType damageType) {
+        if (damageType == DamageInfo.DamageType.NORMAL) {
+            return damage * (1 + (PERCENT_PER * amount)/100f);
+        }
+        return damage;
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + (PERCENT_PER * amount) + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new FrostyPower(owner, amount);
+    }
+}

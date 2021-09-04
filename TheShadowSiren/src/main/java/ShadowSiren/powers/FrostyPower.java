90fb26388aa30df74dd1c35b008dbd76b9c5be3d
Alison Turner <alisonmoons@gmail.com>
2021-09-03 21:40:59 -0300

    Frosty power now is 25% more damage per stack, was 10%

--- TheShadowSiren/src/main/java/ShadowSiren/powers/FrostyPower.java
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/FrostyPower.java
@@ -15,7 +15,7 @@ public class FrostyPower extends AbstractPower implements CloneablePowerInterfac
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    public static final int PERCENT_PER = 10;
+    public static final int PERCENT_PER = 25;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.

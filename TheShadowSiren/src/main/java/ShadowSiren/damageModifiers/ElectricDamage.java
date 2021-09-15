09fc3f3d55becaae9243f5a61265e5cb14e38276
Alison Turner <alisonmoons@gmail.com>
2021-09-15 12:31:35 -0300

    Electric Damage now splashes again for 50% more damage

--- TheShadowSiren/src/main/java/ShadowSiren/damageModifiers/ElectricDamage.java
+++ TheShadowSiren/src/main/java/ShadowSiren/damageModifiers/ElectricDamage.java
@@ -1,32 +1,42 @@
 package ShadowSiren.damageModifiers;
 
+import IconsAddon.damageModifiers.AbstractDamageModifier;
 import IconsAddon.icons.AbstractCustomIcon;
 import IconsAddon.icons.ElectricIcon;
 import IconsAddon.util.DamageModifierHelper;
 import IconsAddon.util.DamageModifierManager;
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.cardModifiers.ChargeModifier;
-import basemod.helpers.CardModifierManager;
+import ShadowSiren.powers.EnergyConversionPower;
+import basemod.BaseMod;
 import basemod.helpers.TooltipInfo;
 import com.badlogic.gdx.graphics.Color;
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.vfx.combat.LightningOrbActivateEffect;
 
 import java.util.ArrayList;
 
 public class ElectricDamage extends AbstractVivianDamageModifier {
+    @SpirePatch(clz = DamageInfo.class, method = SpirePatch.CLASS)
+    public static class LoopCastCheck {
+        public static SpireField<Boolean> loopCast = new SpireField<>(() -> false);
+    }
     public static final String ID = ShadowSirenMod.makeID("ElectricDamage");
+
+    public static final int DIVISOR = 2;
 
     TooltipInfo electricTooltip = null;
     TooltipInfo electricDamageTooltip = null;
     TooltipInfo electricBlockTooltip = null;
+    TooltipInfo shockTooltip = null;
     public boolean innate;
 
     public ElectricDamage() {
@@ -54,14 +64,14 @@ public class ElectricDamage extends AbstractVivianDamageModifier {
 
     @Override
     public void onDamageModifiedByBlock(DamageInfo info, int unblockedAmount, int blockedAmount, AbstractCreature targetHit) {
-        if (targetHit instanceof AbstractMonster) {
-            /*if (unblockedAmount+blockedAmount > 1) {
+        if (targetHit instanceof AbstractMonster && !LoopCastCheck.loopCast.get(info)) {
+            if (unblockedAmount+blockedAmount > 1) {
                 this.addToTop(new AbstractGameAction() {
                     @Override
                     public void update() {
                         targetHit.tint.color = Color.YELLOW.cpy();
                         targetHit.tint.changeColor(Color.WHITE.cpy());
-                        int damage = (unblockedAmount+blockedAmount)/3;
+                        int damage = (unblockedAmount+blockedAmount)/DIVISOR;
                         if (damage == 0) {
                              damage = 1;
                         }
@@ -69,24 +79,22 @@ public class ElectricDamage extends AbstractVivianDamageModifier {
                         if (target != null) {
                             AbstractDungeon.effectList.add(new LightningOrbActivateEffect(target.hb.cX, target.hb.cY));
                             CardCrawlGame.sound.play("ORB_LIGHTNING_CHANNEL", 0.2f);
-                            this.addToTop(new DamageAction(target, DamageModifierHelper.makeBoundDamageInfoFromArray(DamageModifierManager.getDamageMods(info), info.owner, damage, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.NONE, true));
+                            DamageInfo di = DamageModifierHelper.makeBoundDamageInfoFromArray(DamageModifierManager.getDamageMods(info), info.owner, damage, DamageInfo.DamageType.THORNS);
+                            LoopCastCheck.loopCast.set(di, true);
+                            this.addToTop(new DamageAction(target, di, AbstractGameAction.AttackEffect.NONE, true));
                             //this.addToTop(new VFXAction(new LightningOrbActivateEffect(target.hb.cX, target.hb.cY)));
                             //this.addToTop(new SFXAction("ORB_LIGHTNING_CHANNEL", 0.2f));
                         }
                         this.isDone = true;
                     }
                 });
-            }*/
-            this.addToBot(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    CardCrawlGame.sound.play("ORB_LIGHTNING_CHANNEL", 0.1F);
-                    targetHit.tint.color = Color.YELLOW.cpy();
-                    targetHit.tint.changeColor(Color.WHITE.cpy());
-                    ChargeModifier.triggerCharge();
-                    this.isDone = true;
-                }
-            });
+            }
+        }
+        if (info.owner != null) {
+            AbstractPower p = info.owner.getPower(EnergyConversionPower.POWER_ID);
+            if (p != null) {
+                p.onSpecificTrigger();
+            }
         }
     }
 
@@ -102,15 +110,20 @@ public class ElectricDamage extends AbstractVivianDamageModifier {
         if (electricBlockTooltip == null) {
             electricBlockTooltip = new TooltipInfo(cardStrings.DESCRIPTION, cardStrings.EXTENDED_DESCRIPTION[2]);
         }
+        if (shockTooltip == null) {
+            shockTooltip = new TooltipInfo(BaseMod.getKeywordTitle("shadowsiren:shock"), BaseMod.getKeywordDescription("shadowsiren:shock"));
+        }
         switch (tipType) {
             case DAMAGE_AND_BLOCK:
                 l.add(electricTooltip);
+                l.add(shockTooltip);
                 break;
             case DAMAGE:
                 l.add(electricDamageTooltip);
                 break;
             case BLOCK:
                 l.add(electricBlockTooltip);
+                l.add(shockTooltip);
                 break;
         }
         return l;

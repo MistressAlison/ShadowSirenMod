67456042f33d397963bcc638f86636a6b243ba91
Alison Turner <alisonmoons@gmail.com>
2021-09-22 14:10:01 -0300

    Glaciate now deals damage and gives an Ice Shard

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Glaciate.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Glaciate.java
@@ -2,20 +2,18 @@ package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractIceCard;
-import ShadowSiren.cards.interfaces.ModularDescription;
+import ShadowSiren.cards.tempCards.IceShard;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Glaciate extends AbstractIceCard implements ModularDescription {
+public class Glaciate extends AbstractIceCard {
 
 
     // TEXT DECLARATION
@@ -37,52 +35,23 @@ public class Glaciate extends AbstractIceCard implements ModularDescription {
     private static final int COST = 0;
     private static final int DAMAGE = 4;
     private static final int UPGRADE_PLUS_DMG = 2;
-    private static final int HITS = 1;
-    private static final int INCREASE = 1;
+    private static final int CARDS = 1;
 
     // /STAT DECLARATION/
 
     public Glaciate() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = damage = DAMAGE;
-        magicNumber = baseMagicNumber = HITS;
-        secondMagicNumber = baseSecondMagicNumber = INCREASE;
-        isMultiDamage = true;
+        magicNumber = baseMagicNumber = CARDS;
+        this.cardsToPreview = new IceShard();
+        grabPreviewKeywords();
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0 ; i < magicNumber ; i++) {
-            this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
-        }
-        if (m.hasPower(WeakPower.POWER_ID)) {
-            this.addToBot(new AbstractGameAction() {
-                @Override
-                public void update() {
-                    upgradeMagicNumber(secondMagicNumber);
-                    this.isDone = true;
-                }
-            });
-        }
-    }
-
-    private boolean enemyHasWeak() {
-        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
-            if (!aM.isDeadOrEscaped() && aM.hasPower(WeakPower.POWER_ID)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void triggerOnGlowCheck() {
-        super.triggerOnGlowCheck();
-        if (enemyHasWeak()) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        } else {
-            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        }
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
+        this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy(), magicNumber));
     }
 
     // Upgraded stats.
@@ -91,18 +60,9 @@ public class Glaciate extends AbstractIceCard implements ModularDescription {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
+            cardsToPreview.upgrade();
+            rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
-        }
-    }
-
-    @Override
-    public void changeDescription() {
-        if (DESCRIPTION != null) {
-            if (magicNumber > 1) {
-                rawDescription = UPGRADE_DESCRIPTION;
-            } else {
-                rawDescription = DESCRIPTION;
-            }
         }
     }
 }

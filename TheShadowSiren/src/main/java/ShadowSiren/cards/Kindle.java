c61221d25b4a74dc4dcea2f4c111c67ef4da8455
Alison Turner <alisonmoons@gmail.com>
2021-09-28 22:47:56 -0300

    Added Kindle - Gives Vigor and Ashes

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Kindle.java
@@ -0,0 +1,64 @@
+package ShadowSiren.cards;
+
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractFireCard;
+import ShadowSiren.cards.tempCards.Ashes;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.watcher.VigorPower;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Kindle extends AbstractFireCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Kindle.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int VIGOR = 5;
+    private static final int UPGRADE_PLUS_VIGOR = 3;
+
+    // /STAT DECLARATION/
+
+    public Kindle() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.DAMAGE_AND_BLOCK);
+        magicNumber = baseMagicNumber = VIGOR;
+        cardsToPreview = new Ashes();
+        exhaust = true;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new ApplyPowerAction(p, p, new VigorPower(p, magicNumber)));
+        this.addToBot(new MakeTempCardInHandAction(cardsToPreview));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_PLUS_VIGOR);
+            initializeDescription();
+        }
+    }
+}

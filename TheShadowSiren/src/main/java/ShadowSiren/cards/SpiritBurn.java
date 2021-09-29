ae7f5873b8a3e02b50fba4a3f7cb5d5d9288dd81
Alison Turner <alisonmoons@gmail.com>
2021-09-28 22:47:06 -0300

    Added Spirit Burn, deals Fire Shadow and lowers Str

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/SpiritBurn.java
@@ -0,0 +1,85 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractMultiElementCard;
+import ShadowSiren.cards.interfaces.MagicAnimation;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.FireDamage;
+import ShadowSiren.damageModifiers.ShadowDamage;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class SpiritBurn extends AbstractMultiElementCard implements MagicAnimation {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(SpiritBurn.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int STR_DOWN = 1;
+    private static final int UPGRADE_PLUS_STR_DOWN = 1;
+
+    // /STAT DECLARATION/
+
+    public SpiritBurn() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = STR_DOWN;
+        exhaust = true;
+        DamageModifierManager.addModifier(this, new FireDamage());
+        DamageModifierManager.addModifier(this, new ShadowDamage());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        this.addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                m.tint.color = Color.BLACK.cpy();
+                m.tint.changeColor(Color.WHITE.cpy());
+                this.isDone = true;
+            }
+        });
+        this.addToBot(new ApplyPowerAction(m, p, new StrengthPower(m, -this.magicNumber), -this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+//        if (!m.hasPower(ArtifactPower.POWER_ID)) {
+//            this.addToBot(new ApplyPowerAction(m, p, new GainStrengthPower(m, this.magicNumber), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+//        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_STR_DOWN);
+            initializeDescription();
+        }
+    }
+}

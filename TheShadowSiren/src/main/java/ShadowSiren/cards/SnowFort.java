726d2a7220348fc31e5758e671a9e147aa83fe8a
Alison Turner <alisonmoons@gmail.com>
2021-09-28 22:48:40 -0300

    Added Snow Fort - Gains Ice Block and Ice Shard

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/SnowFort.java
@@ -0,0 +1,68 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.BlockModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.blockTypes.IceBlock;
+import ShadowSiren.cards.abstractCards.AbstractIceCard;
+import ShadowSiren.cards.tempCards.IceShard;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class SnowFort extends AbstractIceCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(SnowFort.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.SKILL;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 2;
+    private static final int BLOCK = 12;
+    private static final int UPGRADE_PLUS_BLOCK = 4;
+
+    // /STAT DECLARATION/
+
+    public SnowFort() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
+        baseBlock = block = BLOCK;
+        cardsToPreview = new IceShard();
+        BlockModifierManager.addModifier(this, new IceBlock());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy()));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            //upgradeBaseCost(UPGRADE_COST);
+            upgradeBlock(UPGRADE_PLUS_BLOCK);
+            cardsToPreview.upgrade();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}

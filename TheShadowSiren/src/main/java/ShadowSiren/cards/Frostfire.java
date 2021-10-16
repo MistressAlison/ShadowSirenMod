5ca3aac8fc0049983c21ada74d4e2b15e816540e
Alison Turner <alisonmoons@gmail.com>
2021-10-16 17:24:59 -0300

    Added Frostfire, it's boring

---
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Frostfire.java
@@ -0,0 +1,71 @@
+package ShadowSiren.cards;
+
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.cards.abstractCards.AbstractMultiElementCard;
+import ShadowSiren.cards.interfaces.MagicAnimation;
+import ShadowSiren.characters.Vivian;
+import ShadowSiren.damageModifiers.FireDamage;
+import ShadowSiren.damageModifiers.IceDamage;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static ShadowSiren.ShadowSirenMod.makeCardPath;
+
+public class Frostfire extends AbstractMultiElementCard implements MagicAnimation {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = ShadowSirenMod.makeID(Frostfire.class.getSimpleName());
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
+    // Setting the image as as easy as can possibly be now. You just need to provide the image name
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 4;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int HITS = 2;
+    private static final int UPGRADE_PLUS_HITS = 1;
+
+    // /STAT DECLARATION/
+
+    public Frostfire() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+        magicNumber = baseMagicNumber = HITS;
+        DamageModifierManager.addModifier(this, new FireDamage());
+        DamageModifierManager.addModifier(this, new IceDamage());
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            this.addToBot(new DamageRandomEnemyAction(new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            //upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeMagicNumber(UPGRADE_PLUS_HITS);
+            initializeDescription();
+        }
+    }
+}

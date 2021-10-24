820f8995be6e37211e40929e0a4e4feca45ad43e
Alison Turner <alisonmoons@gmail.com>
2021-10-24 11:34:21 -0300

    Removed unused imports

--- TheShadowSiren/src/main/java/ShadowSiren/cards/ReturningStrike.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ReturningStrike.java
@@ -2,13 +2,9 @@ package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
-import ShadowSiren.cards.tempCards.DireStrike;
-import ShadowSiren.cards.tempCards.TempCard;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;

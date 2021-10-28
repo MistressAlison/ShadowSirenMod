08daac10abd088e06d817d5bfe7f883956077fd1
Alison Turner <alisonmoons@gmail.com>
2021-10-28 11:17:19 -0300

    Recuperate now reduces the cost of a random card in your hand to 0 this turn

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Recuperate.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Recuperate.java
@@ -2,24 +2,19 @@ package ShadowSiren.cards;
 
 import IconsAddon.util.BlockModifierManager;
 import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.actions.ReduceRandomCardToZeroForTurnAction;
 import ShadowSiren.blockTypes.ShadowBlock;
 import ShadowSiren.cards.abstractCards.AbstractShadowCard;
-import ShadowSiren.cards.interfaces.ModularDescription;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
-import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Recuperate extends AbstractShadowCard implements ModularDescription {
+public class Recuperate extends AbstractShadowCard {
 
     // TEXT DECLARATION
 
@@ -36,8 +31,8 @@ public class Recuperate extends AbstractShadowCard implements ModularDescription
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int BLOCK = 5;
-    private static final int UPGRADE_PLUS_BLOCK = 2;
+    private static final int BLOCK = 6;
+    private static final int UPGRADE_PLUS_BLOCK = 3;
     private static final int CARDS = 1;
 
     // /STAT DECLARATION/
@@ -54,38 +49,39 @@ public class Recuperate extends AbstractShadowCard implements ModularDescription
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.addToBot(new GainBlockAction(p, block));
-        int cleansed = 0;
-        for (AbstractPower pow : p.powers) {
-            if (pow.type == AbstractPower.PowerType.DEBUFF && !(pow instanceof InvisiblePower)) {
-                cleansed++;
-                this.addToBot(new RemoveSpecificPowerAction(p, p, pow));
-                if (cleansed == magicNumber) {
-                    break;
-                }
-            }
-        }
-        if (cleansed < magicNumber) {
-            this.addToBot(new DrawCardAction(magicNumber-cleansed));
-        }
+        this.addToBot(new ReduceRandomCardToZeroForTurnAction(p.hand));
+//        int cleansed = 0;
+//        for (AbstractPower pow : p.powers) {
+//            if (pow.type == AbstractPower.PowerType.DEBUFF && !(pow instanceof InvisiblePower)) {
+//                cleansed++;
+//                this.addToBot(new RemoveSpecificPowerAction(p, p, pow));
+//                if (cleansed == magicNumber) {
+//                    break;
+//                }
+//            }
+//        }
+//        if (cleansed < magicNumber) {
+//            this.addToBot(new DrawCardAction(magicNumber-cleansed));
+//        }
     }
-
-    private boolean hasDebuff() {
-        for (AbstractPower pow : AbstractDungeon.player.powers) {
-            if (pow.type == AbstractPower.PowerType.DEBUFF) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void triggerOnGlowCheck() {
-        super.triggerOnGlowCheck();
-        if (hasDebuff()) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        } else {
-            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        }
-    }
+//
+//    private boolean hasDebuff() {
+//        for (AbstractPower pow : AbstractDungeon.player.powers) {
+//            if (pow.type == AbstractPower.PowerType.DEBUFF) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//
+//    public void triggerOnGlowCheck() {
+//        super.triggerOnGlowCheck();
+//        if (hasDebuff()) {
+//            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+//        } else {
+//            this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+//        }
+//    }
 
     //Upgraded stats.
     @Override
@@ -97,14 +93,14 @@ public class Recuperate extends AbstractShadowCard implements ModularDescription
         }
     }
 
-    @Override
-    public void changeDescription() {
-        if (DESCRIPTION != null) {
-            if (magicNumber > 1) {
-                rawDescription = UPGRADE_DESCRIPTION;
-            } else {
-                rawDescription = DESCRIPTION;
-            }
-        }
-    }
+//    @Override
+//    public void changeDescription() {
+//        if (DESCRIPTION != null) {
+//            if (magicNumber > 1) {
+//                rawDescription = UPGRADE_DESCRIPTION;
+//            } else {
+//                rawDescription = DESCRIPTION;
+//            }
+//        }
+//    }
 }

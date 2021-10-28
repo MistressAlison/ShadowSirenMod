087441f521bd6977c4ede83a6a6b4c790d7b4a35
Alison Turner <alisonmoons@gmail.com>
2021-10-28 11:15:50 -0300

    Spirit Drain now an attack

--- TheShadowSiren/src/main/java/ShadowSiren/cards/SpiritDrain.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/SpiritDrain.java
@@ -4,15 +4,17 @@ import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractShadowCard;
 import ShadowSiren.characters.Vivian;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.ArtifactPower;
 import com.megacrit.cardcrawl.powers.GainStrengthPower;
-import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.vfx.combat.CardPoofEffect;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
@@ -21,18 +23,20 @@ public class SpiritDrain extends AbstractShadowCard {
     // TEXT DECLARATION
 
     public static final String ID = ShadowSirenMod.makeID(SpiritDrain.class.getSimpleName());
-    public static final String IMG = makeCardPath("PlaceholderSkill.png");
+    public static final String IMG = makeCardPath("PlaceholderAttack.png");
 
     // /TEXT DECLARATION/
 
     // STAT DECLARATION
 
     private static final AbstractCard.CardRarity RARITY = CardRarity.COMMON;
-    private static final AbstractCard.CardTarget TARGET = CardTarget.ALL;
-    private static final AbstractCard.CardType TYPE = CardType.SKILL;
+    private static final AbstractCard.CardTarget TARGET = CardTarget.ENEMY;
+    private static final AbstractCard.CardType TYPE = CardType.ATTACK;
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
+    private static final int DAMAGE = 5;
+    private static final int UPGRADE_PLUS_DAMAGE = 2;
     private static final int AMOUNT = 2;
     private static final int UPGRADE_PLUS_AMOUNT = 1;
 
@@ -41,28 +45,21 @@ public class SpiritDrain extends AbstractShadowCard {
 
     public SpiritDrain() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        damage = baseDamage = DAMAGE;
         magicNumber = baseMagicNumber = AMOUNT;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        //int sum = 0;
-        for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
-            if (!aM.isDeadOrEscaped()) {
-                this.addToBot(new ApplyPowerAction(aM, p, new StrengthPower(aM, -this.magicNumber), -this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
-                if (!aM.hasPower(ArtifactPower.POWER_ID)) {
-                    this.addToBot(new ApplyPowerAction(aM, p, new GainStrengthPower(aM, this.magicNumber), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
-                    //sum += magicNumber;
-                }
-            }
+        this.addToBot(new VFXAction(new CardPoofEffect(m.hb.cX, m.hb.cY)));
+        this.addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+        this.addToBot(new ApplyPowerAction(m, p, new StrengthPower(m, -this.magicNumber), -this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+        if (!m.hasPower(ArtifactPower.POWER_ID)) {
+            this.addToBot(new ApplyPowerAction(m, p, new GainStrengthPower(m, this.magicNumber), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
         }
-        this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
-        this.addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, magicNumber)));
-//        if (sum > 0) {
-//            this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, sum)));
-//            this.addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, sum)));
-//        }
+        //this.addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, magicNumber)));
+        //this.addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, magicNumber)));
     }
 
     //Upgraded stats.
@@ -70,6 +67,7 @@ public class SpiritDrain extends AbstractShadowCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DAMAGE);
             upgradeMagicNumber(UPGRADE_PLUS_AMOUNT);
             initializeDescription();
         }

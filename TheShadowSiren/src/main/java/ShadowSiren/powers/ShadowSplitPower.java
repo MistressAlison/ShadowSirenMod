182eca15b38c2b19f01ec3c35a96b9f579269bf6
Alison Turner <alisonmoons@gmail.com>
2021-10-28 11:14:29 -0300

    Added new Shadow Split power, works like Pressure Points' Mark

---
+++ TheShadowSiren/src/main/java/ShadowSiren/powers/ShadowSplitPower.java
@@ -0,0 +1,79 @@
+package ShadowSiren.powers;
+
+import IconsAddon.util.DamageModifierManager;
+import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.damageModifiers.ShadowDamage;
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.Color;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.combat.HeartBuffEffect;
+
+public class ShadowSplitPower extends AbstractPower implements CloneablePowerInterface, HealthBarRenderPower {
+
+    public static final String POWER_ID = ShadowSirenMod.makeID("ShadowSplitPower");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private final Color hpBarColor = Color.DARK_GRAY.cpy();
+
+    // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
+    // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
+    //private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("placeholder_power84.png"));
+    //private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("placeholder_power32.png"));
+
+    public ShadowSplitPower(AbstractCreature owner, int amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.amount = amount;
+
+        this.type = PowerType.DEBUFF;
+        this.isTurnBased = false;
+
+        // We load those txtures here.
+        this.loadRegion("unawakened");
+        //logger.info("Blasting Fuse?");
+        //this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
+        //this.region48 = new TextureAtlas.AtlasRegion(tex32, 0, 0, 32, 32);
+
+        updateDescription();
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (DamageModifierManager.getDamageMods(info).stream().anyMatch(m -> m instanceof ShadowDamage)) {
+            this.addToBot(new VFXAction(new HeartBuffEffect(owner.hb.cX, owner.hb.cY), 0.0f));
+            this.addToBot(new LoseHPAction(this.owner, null, this.amount, AbstractGameAction.AttackEffect.NONE));
+        }
+        return damageAmount;
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new ShadowSplitPower(owner, amount);
+    }
+
+    @Override
+    public int getHealthBarAmount() {
+        return amount;
+    }
+
+    @Override
+    public Color getColor() {
+        return hpBarColor;
+    }
+}

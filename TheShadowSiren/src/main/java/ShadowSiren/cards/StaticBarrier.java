8da9ce85f3257550c06ae6cf67c638633546170d
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-05-31 17:10:43 -0300

    Rework StaticBarrier

--- TheShadowSiren/src/main/java/ShadowSiren/cards/StaticBarrier.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/StaticBarrier.java
@@ -1,18 +1,20 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.blockTypes.ElectricBlock;
 import ShadowSiren.cards.abstractCards.AbstractElectricCard;
+import ShadowSiren.cards.interfaces.InHandCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
-import com.evacipated.cardcrawl.mod.stslib.blockmods.BlockModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class StaticBarrier extends AbstractElectricCard {
+public class StaticBarrier extends AbstractElectricCard implements InHandCard {
 
 
     // TEXT DECLARATION
@@ -32,25 +34,30 @@ public class StaticBarrier extends AbstractElectricCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 2;
-    private static final int BLOCK = 5;
-    private static final int UPGRADE_PLUS_BLOCK = 2;
-    private static final int TIMES = 2;
+    private static final int BLOCK = 10;
+    private static final int UPGRADE_PLUS_BLOCK = 3;
+    private static final int RECOIL = 3;
+    private static final int UPGRADE_PLUS_RECOIL = 1;
 
     // /STAT DECLARATION/
 
     public StaticBarrier() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseBlock = block = BLOCK;
-        magicNumber = baseMagicNumber = TIMES;
-        BlockModifierManager.addModifier(this, new ElectricBlock());
+        magicNumber = baseMagicNumber = RECOIL;
         selfRetain = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        for (int i = 0 ; i < magicNumber ; i++) {
-            this.addToBot(new GainBlockAction(p, block));
+        this.addToBot(new GainBlockAction(p, block));
+    }
+
+    @Override
+    public void onDamaged(DamageInfo info) {
+        if (info.owner != null && info.owner != AbstractDungeon.player && info.type == DamageInfo.DamageType.NORMAL) {
+            addToTop(new DamageAction(info.owner, new DamageInfo(AbstractDungeon.player, magicNumber, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_LIGHT, true));
         }
     }
 
@@ -60,6 +67,7 @@ public class StaticBarrier extends AbstractElectricCard {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_RECOIL);
             initializeDescription();
         }
     }

7af41592ac8935300d392879a5b4891989e76cf4
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-05-31 21:42:10 -0300

    Rework ElectroShield

--- TheShadowSiren/src/main/java/ShadowSiren/cards/ElectroShield.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/ElectroShield.java
@@ -1,25 +1,22 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.blockTypes.ElectricBlock;
 import ShadowSiren.cards.abstractCards.AbstractElectricCard;
 import ShadowSiren.characters.Vivian;
-import ShadowSiren.damageModifiers.AbstractVivianDamageModifier;
-import com.evacipated.cardcrawl.mod.stslib.blockmods.BlockModifierManager;
+import ShadowSiren.powers.ShockPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
-public class Shock extends AbstractElectricCard {
+public class ElectroShield extends AbstractElectricCard {
 
 
     // TEXT DECLARATION
 
-    public static final String ID = ShadowSirenMod.makeID(Shock.class.getSimpleName());
+    public static final String ID = ShadowSirenMod.makeID(ElectroShield.class.getSimpleName());
     public static final String IMG = makeCardPath("ElectroShield.png");
     // Setting the image as as easy as can possibly be now. You just need to provide the image name
 
@@ -29,29 +26,29 @@ public class Shock extends AbstractElectricCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.COMMON;
-    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardTarget TARGET = CardTarget.SELF_AND_ENEMY;
     private static final CardType TYPE = CardType.SKILL;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int BLOCK = 2;
+    private static final int BLOCK = 3;
     private static final int UPGRADE_PLUS_BLOCK = 2;
-    private static final int DRAW = 1;
+    private static final int SHOCK = 2;
+    private static final int UPGRADE_PLUS_SHOCK = 1;
 
     // /STAT DECLARATION/
 
-    public Shock() {
-        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET, AbstractVivianDamageModifier.TipType.BLOCK);
+    public ElectroShield() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseBlock = block = BLOCK;
-        magicNumber = baseMagicNumber = DRAW;
-        BlockModifierManager.addModifier(this, new ElectricBlock());
+        magicNumber = baseMagicNumber = SHOCK;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new GainBlockAction(p, block));
-        addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, magicNumber)));
+        addToBot(new GainBlockAction(p, block));
+        addToBot(new ApplyPowerAction(m, p, new ShockPower(m, magicNumber)));
     }
 
     // Upgraded stats.
@@ -60,6 +57,7 @@ public class Shock extends AbstractElectricCard {
         if (!upgraded) {
             upgradeName();
             upgradeBlock(UPGRADE_PLUS_BLOCK);
+            upgradeMagicNumber(UPGRADE_PLUS_SHOCK);
             initializeDescription();
         }
     }

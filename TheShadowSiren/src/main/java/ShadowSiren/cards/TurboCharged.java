24b9789a5769d0a5b5ddc4d98e9b01fdf1796539
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-01 13:09:38 -0300

    Refactor TurboCharged

--- TheShadowSiren/src/main/java/ShadowSiren/cards/TurboCharged.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/TurboCharged.java
@@ -1,14 +1,15 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.actions.EasyXCostAction;
 import ShadowSiren.cards.abstractCards.AbstractElectricCard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.ChargePower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.relics.ChemicalX;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import java.util.Arrays;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
@@ -29,46 +30,27 @@ public class TurboCharged extends AbstractElectricCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = -1;
-    private static final int BASE_EFFECT = 0;
     private static final int MULTI = 4;
 
     // /STAT DECLARATION/
 
     public TurboCharged() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-//        secondMagicNumber = baseSecondMagicNumber = BASE_EFFECT;
+        baseMagicNumber = magicNumber = MULTI;
         exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        int effect = energyOnUse;
-
-        if (p.hasRelic(ChemicalX.ID)) {
-            effect += ChemicalX.BOOST;
-        }
-
-        if (effect > 0) {
-            this.addToBot(new ApplyPowerAction(p, p, new ChargePower(p, effect * MULTI)));
-        }
-
-        if (!this.freeToPlayOnce) {
-            p.energy.use(EnergyPanel.totalCount);
-        }
+        addToBot(new EasyXCostAction(this, (base, params) -> {
+            int effect = base + Arrays.stream(params).sum();
+            if (effect > 0) {
+                addToTop(new ApplyPowerAction(p, p, new ChargePower(p, effect * magicNumber)));
+            }
+            return true;
+        }));
     }
-
-//    @Override
-//    public void applyPowers() {
-//        super.applyPowers();
-//        updateSecondValue();
-//        initializeDescription();
-//    }
-//
-//    private void updateSecondValue() {
-//        secondMagicNumber = XCostGrabber.getXCostAmount(this, true)*MULTI;
-//        isSecondMagicNumberModified = secondMagicNumber != baseSecondMagicNumber;
-//    }
 
     //Upgraded stats.
     @Override

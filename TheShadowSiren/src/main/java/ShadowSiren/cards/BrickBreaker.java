32ae3af197683d93ea1626901ef33919feae0586
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-03 13:25:53 -0300

    BrickBreaker no longer applies Weak, cards increased 2 -> 3

--- TheShadowSiren/src/main/java/ShadowSiren/cards/BrickBreaker.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BrickBreaker.java
@@ -4,11 +4,9 @@ import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractIceCard;
 import ShadowSiren.cards.tempCards.IceShard;
 import ShadowSiren.characters.Vivian;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.WeakPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
@@ -32,7 +30,7 @@ public class BrickBreaker extends AbstractIceCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 1;
-    private static final int CARDS = 2;
+    private static final int CARDS = 3;
     private static final int UPGRADE_PLUS_CARDS = 1;
 
     // /STAT DECLARATION/
@@ -47,7 +45,7 @@ public class BrickBreaker extends AbstractIceCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new ApplyPowerAction(m, p, new WeakPower(m, magicNumber, false)));
+        //this.addToBot(new ApplyPowerAction(m, p, new WeakPower(m, magicNumber, false)));
         this.addToBot(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy(), magicNumber));
     }
 

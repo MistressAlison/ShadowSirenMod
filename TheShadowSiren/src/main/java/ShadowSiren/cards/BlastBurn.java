988171b4592ea3377dd0b0d5dd016740af1e64f5
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-04 21:35:29 -0300

    BlastBurn no longer AOE

--- TheShadowSiren/src/main/java/ShadowSiren/cards/BlastBurn.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/BlastBurn.java
@@ -1,13 +1,17 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
-import ShadowSiren.actions.BlastBurnAction;
 import ShadowSiren.cards.abstractCards.AbstractFireCard;
 import ShadowSiren.cards.tempCards.Ashes;
 import ShadowSiren.characters.Vivian;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ExplosionSmallEffect;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
@@ -26,7 +30,7 @@ public class BlastBurn extends AbstractFireCard {
     // STAT DECLARATION
 
     private static final CardRarity RARITY = CardRarity.RARE;
-    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
     private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
@@ -43,15 +47,17 @@ public class BlastBurn extends AbstractFireCard {
         baseDamage = damage = DAMAGE;
         magicNumber = baseMagicNumber = ASHES;
         cardsToPreview = new Ashes();
-        isMultiDamage = true;
         exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new BlastBurnAction(p, multiDamage, damageTypeForTurn));
-        this.addToBot(new MakeTempCardInHandAction(cardsToPreview, magicNumber));
+        if (m != null) {
+            addToBot(new VFXAction(new ExplosionSmallEffect(m.hb.cX, m.hb.cY)));
+        }
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE));
+        addToBot(new MakeTempCardInHandAction(cardsToPreview, magicNumber));
     }
 
     // Upgraded stats.

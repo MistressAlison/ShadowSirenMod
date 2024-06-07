8bbc748d7cefae025170311b4d85efbfb33f7cb7
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-07 18:37:19 -0300

    Singe rework, now applies Burn and Intensifies

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Singe.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Singe.java
@@ -1,10 +1,11 @@
 package ShadowSiren.cards;
 
 import ShadowSiren.ShadowSirenMod;
+import ShadowSiren.actions.IntensifyAction;
 import ShadowSiren.cards.abstractCards.AbstractFireCard;
 import ShadowSiren.characters.Vivian;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import ShadowSiren.powers.BurningPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -28,28 +29,24 @@ public class Singe extends AbstractFireCard {
     public static final AbstractCard.CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int HP_LOSS = 5;
-    private static final int UPGRADE_PLUS_HP_LOSS = 2;
+    private static final int EFFECT = 3;
+    private static final int UPGRADE_PLUS_EFFECT = 2;
+    private static final int SCALE = 1;
 
     // /STAT DECLARATION/
 
 
     public Singe() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-        magicNumber = baseMagicNumber = HP_LOSS;
+        magicNumber = baseMagicNumber = EFFECT;
+        baseSecondMagicNumber = secondMagicNumber = SCALE;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new LoseHPAction(m, p, magicNumber, AbstractGameAction.AttackEffect.FIRE));
-        this.addToBot(new AbstractGameAction() {
-            @Override
-            public void update() {
-                m.decreaseMaxHealth(magicNumber*2);
-                this.isDone = true;
-            }
-        });
+        addToBot(new ApplyPowerAction(m, p, new BurningPower(m, magicNumber)));
+        addToBot(new IntensifyAction(this, secondMagicNumber, IntensifyAction.EffectType.MAGIC));
     }
 
     //Upgraded stats.
@@ -57,7 +54,7 @@ public class Singe extends AbstractFireCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_PLUS_HP_LOSS);
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
         }
     }
 }

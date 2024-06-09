c16dfb350225b3612536b094656a7ccc98f71a52
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-09 18:20:21 -0300

    FlashFreeze rework, now applies 2(3)X Chill and Exhausts

--- TheShadowSiren/src/main/java/ShadowSiren/cards/FlashFreeze.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/FlashFreeze.java
@@ -3,12 +3,10 @@ package ShadowSiren.cards;
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.actions.EasyXCostAction;
 import ShadowSiren.cards.abstractCards.AbstractIceCard;
-import ShadowSiren.cards.tempCards.IceShard;
 import ShadowSiren.characters.Vivian;
 import ShadowSiren.powers.ChillPower;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -38,23 +36,25 @@ public class FlashFreeze extends AbstractIceCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = -1;
+    private static final int EFFECT = 2;
+    private static final int UPGRADE_PLUS_EFFECT = 1;
 
     // /STAT DECLARATION/
 
     public FlashFreeze() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
-
-        this.cardsToPreview = new IceShard();
+        baseMagicNumber = magicNumber = EFFECT;
+        exhaust = true;
     }
 
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new EasyXCostAction(this, (base, params) -> {
-            int effect = base + Arrays.stream(params).sum();
+            int effect = (base + Arrays.stream(params).sum()) * magicNumber;
             if (effect > 0) {
                 AbstractDungeon.effectsQueue.add(new BorderFlashEffect(Color.WHITE.cpy(), true));
-                addToTop(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy(), effect));
+                //addToTop(new MakeTempCardInHandAction(cardsToPreview.makeStatEquivalentCopy(), effect));
                 for (AbstractMonster aM : AbstractDungeon.getMonsters().monsters) {
                     if (!aM.isDeadOrEscaped()) {
                         addToTop(new ApplyPowerAction(aM, p, new ChillPower(aM, effect), effect, true));
@@ -72,8 +72,7 @@ public class FlashFreeze extends AbstractIceCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            cardsToPreview.upgrade();
-            rawDescription = UPGRADE_DESCRIPTION;
+            upgradeMagicNumber(UPGRADE_PLUS_EFFECT);
             initializeDescription();
         }
     }

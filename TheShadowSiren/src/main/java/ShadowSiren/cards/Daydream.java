9d6c92b2c634cb35dbef0c5fc715bda8d0c8e43d
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-06-10 19:51:05 -0300

    Daydream now draws cards next turn, draw increased 2(3) -> 3(4)

--- TheShadowSiren/src/main/java/ShadowSiren/cards/Daydream.java
+++ TheShadowSiren/src/main/java/ShadowSiren/cards/Daydream.java
@@ -3,9 +3,10 @@ package ShadowSiren.cards;
 import ShadowSiren.ShadowSirenMod;
 import ShadowSiren.cards.abstractCards.AbstractDynamicCard;
 import ShadowSiren.characters.Vivian;
-import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
 
 import static ShadowSiren.ShadowSirenMod.makeCardPath;
 
@@ -27,7 +28,7 @@ public class Daydream extends AbstractDynamicCard {
     public static final CardColor COLOR = Vivian.Enums.VOODOO_CARD_COLOR;
 
     private static final int COST = 0;
-    private static final int CARDS = 2;
+    private static final int CARDS = 3;
     private static final int UPGRADE_PLUS_CARDS = 1;
 
     // /STAT DECLARATION/
@@ -41,7 +42,8 @@ public class Daydream extends AbstractDynamicCard {
     // Actions the card should do.
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.addToBot(new DrawCardAction(p, magicNumber));
+        //this.addToBot(new DrawCardAction(p, magicNumber));
+        addToBot(new ApplyPowerAction(p, p, new DrawCardNextTurnPower(p, magicNumber)));
     }
 
     //Upgraded stats.
